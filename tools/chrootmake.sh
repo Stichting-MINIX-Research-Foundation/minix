ec96454af3ee928848aacec327fd16970d24df27
Ben Gras <ben@minix3.org>
2005-04-22 16:16:23 +0000

    Fixing make install and chroot make process; revision should not be part of cvs

--- tools/chrootmake.sh
+++ tools/chrootmake.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
-
-DIST=/usr/dist
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-
+cd /usr || exit 1
+tar cf src.tar src || exit 1
 cd /usr/src || exit 1
-mkdir -p $DIST
-#echo "Making src.tar"
-#tar cf /dist/src.tar /usr/src || exit 1
 make world install || exit 1
-
 cd tools || exit 1
 make hdboot
-
-#rm -rf /usr/src
-#echo "Making bin.tar"
-#tar cf /dist/bin.tar /
-
+cd /usr || exit 1
+rm -rf /usr/src || exit 1
 exit 0
 
