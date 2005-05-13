49b86f337383d9f5097f2abfb70f78aa1a95a721
Jorrit Herder <jnherder@minix3.org>
2005-05-13 09:33:53 +0000

    Fixed tick_delay; it now reschedules a previous synchronous alarm.

--- lib/utils/tick_delay.c
+++ lib/utils/tick_delay.c
@@ -6,7 +6,14 @@
 PUBLIC int tick_delay(ticks)
 long ticks;				/* number of ticks to wait */
 {
-    message m;
+/* This function uses the synchronous alarm to delay for a while. This works
+ * even if a previous synchronous alarm was scheduled, because the remaining
+ * tick of the previous alarm are returned so that it can be rescheduled.
+ * Note however that a long tick_delay (longer than the remaining time of the
+ * previous) alarm will also delay the previous alarm.
+ */
+    message m, m_alarm;
+    clock_t time_left;
     int s;
 
     if (ticks <= 0) return;		/* check for robustness */
@@ -16,10 +23,20 @@ long ticks;				/* number of ticks to wait */
     m.ALRM_EXP_TIME = ticks;		/* request message after ticks */
     m.ALRM_ABS_TIME = 0;		/* ticks are relative to now */
     s = _taskcall(SYSTASK, SYS_SYNCALRM, &m);
+    if (s != OK) return(s);
 
-    if (OK == s) receive(HARDWARE,&m);	/* await synchronous alarm */
+    receive(HARDWARE,&m_alarm);		/* await synchronous alarm */
+
+    /* Check if we must reschedule the current alarm. */
+    if (m.ALRM_TIME_LEFT > 0) {
+    	printf("tick_delay: reschedule alarm\n");
+    	m.ALRM_EXP_TIME = m.ALRM_TIME_LEFT - ticks;
+    	if (m.ALRM_EXP_TIME <= 0) 
+    		m.ALRM_EXP_TIME = 1;
+    	s = _taskcall(SYSTASK, SYS_SYNCALRM, &m);
+    }
+
     return(s);
-
 }
 
 
