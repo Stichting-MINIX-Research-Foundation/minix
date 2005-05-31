f0af8d85e7776f0fe047c2ed882f13d07eadf662
Jorrit Herder <jnherder@minix3.org>
2005-05-31 14:44:49 +0000

    *** empty log message ***

---
+++ lib/utils/get_upt.c
@@ -0,0 +1,22 @@
+#include "utils.h"
+
+/*===========================================================================*
+ *                               getuptime			    	     *
+ *===========================================================================*/
+PUBLIC int getuptime(ticks)
+clock_t *ticks;				/* uptime in ticks */
+{
+    message m;
+    int s;
+
+    m.m_type = SYS_TIMES;		/* request time information */
+    m.T_PROC_NR = NONE;			/* ignore process times */
+    s = _taskcall(SYSTASK, SYS_TIMES, &m);
+    *ticks = m.T_BOOT_TICKS;
+    return(s);
+}
+
+
+
+
+
