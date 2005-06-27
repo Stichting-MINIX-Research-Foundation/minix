b05fa195d6de5ba0f8b1d1aabc885ec1eb3f334c
Ben Gras <ben@minix3.org>
2005-06-27 09:30:49 +0000

    Prototype and definitions for getopt()

--- include/unistd.h
+++ include/unistd.h
@@ -167,5 +167,7 @@ _PROTOTYPE( int setgroups, (int ngroups, const gid_t *gidset)		);
 #endif
 
 _PROTOTYPE( int readlink, (const char *, char *, int));
+_PROTOTYPE( int getopt, (int, char **, char *));
+extern int optind, opterr, optopt;
 
 #endif /* _UNISTD_H */
