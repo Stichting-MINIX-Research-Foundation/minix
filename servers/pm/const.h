3e5276eff7e70f8c38b94e26118b747a56bffd7c
Philip Homburg <philip@cs.vu.nl>
2005-06-30 12:56:34 +0000

    changed max pid back to 30000

--- servers/pm/const.h
+++ servers/pm/const.h
@@ -11,7 +11,10 @@
 #define MAX_PAGES       4096	/* how many pages in the virtual addr space */
 #endif
 
-#define NR_PIDS	       65536	/* process ids range from 0 to NR_PIDS-1 */
+#define NR_PIDS	       30000	/* process ids range from 0 to NR_PIDS-1.
+				 * (magic constant: some old applications use
+				 * a 'short' instead of pid_t.)
+				 */
 
 #define PM_PID	           0	/* PM's process id number */
 #define PM_PARENT         -1	/* PM's parent process slot */
