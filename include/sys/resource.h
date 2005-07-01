83f0f501f8f3c69d6be98f0ad715a5052fc05cd0
Ben Gras <ben@minix3.org>
2005-07-01 09:59:49 +0000

    Added <sys/resource.h> for [gs]etpriority() call.

---
+++ include/sys/resource.h
@@ -0,0 +1,19 @@
+
+#ifndef _SYS_RESOURCE_H
+#define _SYS_RESOURCE_H
+
+/* Priority range for the get/setpriority() interface.
+ * It isn't a mapping on the internal minix scheduling
+ * priority.
+ */
+#define PRIO_MIN	-20
+#define PRIO_MAX	 20
+
+#define PRIO_PROCESS	0
+#define PRIO_PGRP	1
+#define PRIO_USER	2
+
+int getpriority(int, int);
+int setpriority(int, int, int);
+
+#endif
