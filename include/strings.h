df1dbb4b7e2ab16b315dd2bbad783bcd4491818b
Philip Homburg <philip@cs.vu.nl>
2005-07-27 11:44:26 +0000

    Prototypes for some string functions that are not part of the C standard.

---
+++ include/strings.h
@@ -0,0 +1,9 @@
+/*
+strings.h
+*/
+
+/* Open Group Base Specifications Issue 6 (not complete) */
+_PROTOTYPE( char *index, (const char *_s, int _charwanted)		);
+_PROTOTYPE( int strcasecmp, (const char *_s1, const char *_s2)		);
+_PROTOTYPE( int strncasecmp, (const char *_s1, const char *_s2,
+							size_t _len)	);
