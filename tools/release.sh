0d583c4f00025e0cc134996363b82946d4fbeba4
Ben Gras <ben@minix3.org>
2005-08-12 12:35:45 +0000

    *** empty log message ***

--- tools/release.sh
+++ tools/release.sh
@@ -48,7 +48,7 @@ mkdir -m 755 $RELEASEDIR/usr
 mkdir -m 1777 $RELEASEDIR/tmp
 
 echo " * Zeroing $TMPDISK"
-#dd if=/dev/zero of=$TMPDISK bs=$BS count=$USRBLOCKS
+dd if=/dev/zero of=$TMPDISK bs=$BS count=$USRBLOCKS
 mkfs -B $BS -b $USRBLOCKS $TMPDISK || exit
 echo " * Mounting $TMPDISK as $RELEASEDIR/usr"
 mount $TMPDISK $RELEASEDIR/usr || exit
