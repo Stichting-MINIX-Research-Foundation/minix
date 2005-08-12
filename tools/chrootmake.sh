98269fe49404df5e60191a5cd44634d117e50d77
Ben Gras <ben@minix3.org>
2005-08-12 17:52:32 +0000

    depend fix

--- tools/chrootmake.sh
+++ tools/chrootmake.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 cd /usr/src || exit 1
-make depend || exit 1
 make world install || exit 1
 cd tools || exit 1
 rm revision
