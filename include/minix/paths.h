d48770fcb5c545da4f1242927a94b437e2372332
Ben Gras <ben@minix3.org>
2005-08-26 12:00:43 +0000

    Path config, so they're not hard-coded separately in commands

---
+++ include/minix/paths.h
@@ -0,0 +1,12 @@
+#ifndef _MINIX_PATHS_H
+#define _MINIX_PATHS_H 1
+
+#define _PATH_DHCPCONF	"/etc/dhcp.conf"
+#define _PATH_DHCPPID	"/usr/run/dhcpd.pid"
+#define _PATH_DHCPCACHE	"/usr/adm/dhcp.cache"
+#define _PATH_DHCPPOOL	"/usr/adm/dhcp.pool"
+
+#define _PATH_WTMP	"/usr/adm/wtmp"
+#define _PATH_LASTLOG	"/usr/adm/lastlog"
+
+#endif
