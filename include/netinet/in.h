f67ccc7333c5527933ca3427d2fc205d40ddc7e9
Ben Gras <ben@minix3.org>
2005-09-01 15:24:05 +0000

    Need an IPPROTO_UDP

--- include/netinet/in.h
+++ include/netinet/in.h
@@ -29,6 +29,7 @@ typedef uint8_t		sa_family_t;
 /* Protocols */
 #define IPPROTO_IP	0	/* Dummy protocol */
 #define IPPROTO_TCP	6	/* TCP */
+#define IPPROTO_UDP	17	/* UDP */
 
 /* setsockopt options at IP level */
 #define IP_ADD_MEMBERSHIP	12
