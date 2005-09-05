c28d7c9d8e7f930a9063d76f1712f99d0a0a2e26
Ben Gras <ben@minix3.org>
2005-09-05 13:03:26 +0000

    Don't print output

--- commands/scripts/minixsize.sh
+++ commands/scripts/minixsize.sh
@@ -3,9 +3,9 @@ t=/usr/src/etc/binary_sizes
 if [ "$1" = big ]
 then t=$t.big
 fi
-chmem =250000 /usr/lib/* /usr/lib/i386/*
+chmem =250000 /usr/lib/* /usr/lib/i386/* >/dev/null 2>&1
 if [ -f $t ]
 then	cat "$t" | while read line
-	do	awk '{ print "chmem =" $2 " " $1 }'
+	do	awk '{ print "chmem =" $2 " " $1 " >/dev/null 2>&1 "}'
 	done | /bin/sh
 fi
