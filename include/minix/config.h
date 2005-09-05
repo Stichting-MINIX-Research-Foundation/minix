d9f669a326c60486928953c4d8819b31c2162424
Ben Gras <ben@minix3.org>
2005-09-05 17:03:24 +0000

    We're nearly at 3.0.8 now..

--- include/minix/config.h
+++ include/minix/config.h
@@ -3,7 +3,7 @@
 
 /* Minix release and version numbers. */
 #define OS_RELEASE "3"
-#define OS_VERSION "0.7"
+#define OS_VERSION "0.8"
 
 /* This file sets configuration parameters for the MINIX kernel, FS, and PM.
  * It is divided up into two main sections.  The first section contains
