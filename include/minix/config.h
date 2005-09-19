e890db422c7191c5121e0cc211f0ab8950ff9e6a
Ben Gras <ben@minix3.org>
2005-09-19 13:56:42 +0000

    new version for PH test

--- include/minix/config.h
+++ include/minix/config.h
@@ -3,7 +3,7 @@
 
 /* Minix release and version numbers. */
 #define OS_RELEASE "3"
-#define OS_VERSION "0.8h"
+#define OS_VERSION "0.9a"
 
 /* This file sets configuration parameters for the MINIX kernel, FS, and PM.
  * It is divided up into two main sections.  The first section contains
