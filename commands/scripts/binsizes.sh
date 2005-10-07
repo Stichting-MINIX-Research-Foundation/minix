fff6f5aeadd8bb176b99083f7a4c1d83f0a9451c
Ben Gras <ben@minix3.org>
2005-10-07 13:50:43 +0000

    Make binsizes work if src isn't in /etc/src. Also include /usr/lib/ego/* files.

--- commands/scripts/binsizes.sh
+++ commands/scripts/binsizes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-t=/usr/src/etc/binary_sizes
+t=/etc/binary_sizes
 
 if [ "$#" -ne 1 ]
 then	echo "Usage: $0 <big|normal>"
@@ -10,7 +10,7 @@ fi
 if [ "$1" = big ]
 then t=$t.big
 fi
-chmem =250000 /usr/lib/* /usr/lib/i386/* >/dev/null 2>&1
+chmem =350000 /usr/lib/* /usr/lib/i386/* /usr/lib/ego/* >/dev/null 2>&1
 if [ -f $t ]
 then	cat "$t" | while read line
 	do	awk '{ print "chmem =" $2 " " $1 " >/dev/null 2>&1 "}'
