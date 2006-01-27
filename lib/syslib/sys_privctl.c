8ec6f7af830966f99d73e6ae93fce1575820dda9
Philip Homburg <philip@cs.vu.nl>
2006-01-27 14:14:13 +0000

    Make sys_privctl make the prototype.

--- lib/syslib/sys_privctl.c
+++ lib/syslib/sys_privctl.c
@@ -1,13 +1,13 @@
 #include "syslib.h"
 
-int sys_privctl(int proc, int request, int i, vir_bytes p)
+int sys_privctl(int proc, int request, int i, void *p)
 {
   message m;
 
   m.CTL_PROC_NR = proc;
   m.CTL_REQUEST = request;
   m.CTL_MM_PRIV = i;
-  m.CTL_ARG_PTR = (char *) p;
+  m.CTL_ARG_PTR = p;
 
-  return _taskcall(SYSTASK, _SYS_PRIVCTL, &m);
+  return _taskcall(SYSTASK, SYS_PRIVCTL, &m);
 }
