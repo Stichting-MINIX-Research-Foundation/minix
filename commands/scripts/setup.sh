42103a43c27d2234477d0e12803e2f8b6c12cce7
Ben Gras <ben@minix3.org>
2006-03-17 20:50:27 +0000

    *** empty log message ***

--- commands/scripts/setup.sh
+++ commands/scripts/setup.sh
@@ -640,19 +640,6 @@ fi
 echo "Saving random data.."
 dd if=/dev/random of=/mnt/usr/adm/random.dat bs=1024 count=1
 
-# Now chroot-mount the new system and run the postinstall script
-umount /dev/$usr || exit 1
-if umount ${cddrive}p2
-then
-	mount /dev/$root /mnt || exit 1
-	mount /dev/$usr /mnt/usr || exit 1
-	echo "Install is done. Running postinstall script.."
-	chroot /mnt '/bin/sh /usr/src/commands/scripts/postinstall.sh'
-	echo "Postinstall script done."
-	mount -r ${cddrive}p2 /usr
-else	echo "Skipping post-install."
-fi
-
 echo "
 Please type 'shutdown' to exit MINIX 3 and enter the boot monitor. At
 the boot monitor prompt, type 'boot $bios', where X is the bios drive
