05fc5023741701255f75676cf778acc6f8631451
Ben Gras <ben@minix3.org>
2006-03-26 19:33:48 +0000

    Intr doesn't work here.

--- commands/scripts/packman.sh
+++ commands/scripts/packman.sh
@@ -119,8 +119,8 @@ then	if [ -f $LISTFILE ]
 		y=y
 	fi
 	if [ "$y" = y -o "$y" = Y ]
-	then	echo "Fetching package list (^C to abort)."
-		intr urlget $LISTURL >$TMPF && mv $TMPF $LISTFILE || echo "Update not successful."
+	then	echo "Fetching package list."
+		urlget $LISTURL >$TMPF && mv $TMPF $LISTFILE || echo "Update not successful."
 	fi
 	netpackages=$LISTFILE
 	if [ ! -f "$netpackages" -o ! `cat "$netpackages" 2>/dev/null | wc -l | awk '{ print $1 }'` -gt 1 ]
