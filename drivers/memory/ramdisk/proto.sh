261ae3b4633c0c7a687202535f9974197b643530
Ben Gras <ben@minix3.org>
2006-04-03 19:36:15 +0000

    Use /dev instead of MAKEDEV because release builds are run as bin.

--- drivers/memory/ramdisk/proto.sh
+++ drivers/memory/ramdisk/proto.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-rm -rf t
-mkdir t 2>/dev/null || true
-cd t
-MAKEDEV std 
-rm fd1* fd0p* tcp* eth* ip* udp* tty[pq]* pty*
-sed -n '1,/@DEV/p' <../proto  | grep -v @DEV@
-ls -aln | grep '^[bc]' | \
+sed -n '1,/@DEV/p' <proto  | grep -v @DEV@
+(
+cd /dev
+ls -aln | grep '^[bc]' | egrep -v ' (fd1|fd0p|tcp|eth|ip|udp|tty[pq]|pty)' | \
 sed	-e 's/^[bc]/& /' -e 's/rw-/6/g' -e 's/r--/4/g' \
 	-e 's/-w-/2/g'  -e 's/---/0/g' | \
 awk '{ printf "\t\t%s %s--%s %d %d %d %d \n", $11, $1, $2, $4, $5, $6, $7; }'
-sed -n '/@DEV/,$p' <../proto  | grep -v @DEV@
+)
+sed -n '/@DEV/,$p' <proto  | grep -v @DEV@
