3ea083b8d4133aa903d23e11cdacabd37979bee6
Philip Homburg <philip@cs.vu.nl>
2006-05-19 12:45:55 +0000

    Actually remove allocmem and freemem sources from the library.

--- lib/syscall/allocmem.s
+++
@@ -1,7 +0,0 @@
-.sect .text
-.extern	__allocmem
-.define	_allocmem
-.align 2
-
-_allocmem:
-	jmp	__allocmem
--- lib/syscall/freemem.s
+++
@@ -1,7 +0,0 @@
-.sect .text
-.extern	__freemem
-.define	_freemem
-.align 2
-
-_freemem:
-	jmp	__freemem
