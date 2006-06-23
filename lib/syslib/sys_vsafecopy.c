31318a8ce56848e346bc79ff6c4b941a78451b60
Ben Gras <ben@minix3.org>
2006-06-23 11:54:35 +0000

    library interface for vectored safecopy variant.

---
+++ lib/syslib/sys_vsafecopy.c
@@ -0,0 +1,18 @@
+
+#include "syslib.h"
+
+#include <minix/safecopies.h>
+
+PUBLIC int sys_vsafecopy(struct vscp_vec *vec, int els)
+{
+/* Vectored variant of sys_safecopy*. */
+
+  message copy_mess;
+
+  copy_mess.VSCP_VEC_ADDR = vec;
+  copy_mess.VSCP_VEC_SIZE = els;
+
+  return(_taskcall(SYSTASK, SYS_VSAFECOPY, &copy_mess));
+
+}
+
