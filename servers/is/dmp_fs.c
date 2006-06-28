46646a49a87fc3e8841461a1fa0fa61e155b05fb
Ben Gras <ben@minix3.org>
2006-06-28 10:04:32 +0000

    Don't print cloexec

--- servers/is/dmp_fs.c
+++ servers/is/dmp_fs.c
@@ -28,19 +28,18 @@ PUBLIC void fproc_dmp()
   getsysinfo(FS_PROC_NR, SI_PROC_TAB, fproc);
 
   printf("File System (FS) process table dump\n");
-  printf("-nr- -pid- -tty- -umask- --uid-- --gid-- -ldr- -sus-rev-proc- -cloexec-\n");
+  printf("-nr- -pid- -tty- -umask- --uid-- --gid-- -ldr- -sus-rev-proc-\n");
   for (i=prev_i; i<NR_PROCS; i++) {
   	fp = &fproc[i];
   	if (fp->fp_pid <= 0) continue;
   	if (++n > 22) break;
-  	printf("%3d  %4d  %2d/%d  0x%05x %2d (%d)  %2d (%d)  %3d   %3d %3d %4d    0x%05x\n",
+  	printf("%3d  %4d  %2d/%d  0x%05x %2d (%d)  %2d (%d)  %3d   %3d %3d %4d\n",
   		i, fp->fp_pid, 
   		((fp->fp_tty>>MAJOR)&BYTE), ((fp->fp_tty>>MINOR)&BYTE), 
   		fp->fp_umask,
   		fp->fp_realuid, fp->fp_effuid, fp->fp_realgid, fp->fp_effgid,
   		fp->fp_sesldr,
-  		fp->fp_suspended, fp->fp_revived, fp->fp_task,
-  		fp->fp_cloexec
+  		fp->fp_suspended, fp->fp_revived, fp->fp_task
   	);
   }
   if (i >= NR_PROCS) i = 0;
