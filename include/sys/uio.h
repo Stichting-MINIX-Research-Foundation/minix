6dbca440306b05074aa0b58b20b0d646d408a91a
Ben Gras <ben@minix3.org>
2006-08-02 15:18:49 +0000

    Turn off prototypes for readv and writev until they're defined (properly) in the library.

--- include/sys/uio.h
+++ include/sys/uio.h
@@ -15,9 +15,11 @@ struct iovec
 	size_t	iov_len;
 };
 
+#if 0
 _PROTOTYPE(ssize_t readv, (int _fildes, const struct iovec *_iov,
 							int _iovcnt)	);
 _PROTOTYPE(ssize_t writev, (int _fildes, const struct iovec *_iov,
 							int iovcnt)	);
+#endif
 
 #endif /* _SYS_UIO_H */
