dfaf30e54dc3219465cecf7f3ea2f7bf0a4d91d2
Ben Gras <ben@minix3.org>
2006-12-22 15:50:17 +0000

    interface.h is unused (remainder from Ingmar's work)

--- kernel/interface.h
+++
@@ -1,9 +0,0 @@
-/* minix/kernel/interface.h - All interfaces the kernel needs.  */
-
-#ifndef _KERNEL_IF_H_
-#define _KERNEL_IF_H_
-
-#include <minix/type.h>
-#include <archtypes.h>
-
-#endif /* #ifndef _KERNEL_IF_H_ */
