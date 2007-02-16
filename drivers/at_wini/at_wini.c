49d2195722c408ba4eb951f0117778c0665323e6
Ben Gras <ben@minix3.org>
2007-02-16 15:56:00 +0000

    made default wakeup time correct

--- drivers/at_wini/at_wini.c
+++ drivers/at_wini/at_wini.c
@@ -427,14 +427,14 @@ PRIVATE void init_params()
   struct wini *wn;
   u8_t params[16];
   int s;
-  long wakeup_secs;
+  long wakeup_secs = WAKEUP_SECS;
 
   /* Boot variables. */
   env_parse("ata_std_timeout", "d", 0, &w_standard_timeouts, 0, 1);
   env_parse("ata_pci_debug", "d", 0, &w_pci_debug, 0, 1);
   env_parse("ata_instance", "d", 0, &w_instance, 0, 8);
   env_parse("ata_no_dma", "d", 0, &disable_dma, 0, 1);
-  env_parse("ata_id_timeout", "d", 0, &wakeup_secs, 1, 60);
+  env_parse("ata_id_timeout", "d", WAKEUP_SECS, &wakeup_secs, 1, 60);
   env_parse("atapi_debug", "d", 0, &atapi_debug, 0, 1);
 
   w_identify_wakeup_ticks = wakeup_secs * HZ;
