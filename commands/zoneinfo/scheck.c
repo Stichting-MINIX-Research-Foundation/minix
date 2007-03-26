3b2c65e3234c6185942ec21ad1ba346000c69368
Ben Gras <ben@minix3.org>
2007-03-26 10:55:16 +0000

    Import of original zoneinfo code and database - tzcode old-tzcode-32-bit-output and tzdata2007d.

---
+++ commands/zoneinfo/scheck.c
@@ -0,0 +1,64 @@
+#
+
+/*LINTLIBRARY*/
+
+#include "stdio.h"
+
+#ifndef lint
+#ifndef NOID
+static char	sccsid[] = "@(#)scheck.c	7.15";
+#endif /* !NOID */
+#endif /* !lint */
+
+#include "ctype.h"
+
+extern char *	imalloc();
+
+char *
+scheck(string, format)
+char *	string;
+char *	format;
+{
+	register char *	fbuf;
+	register char *	fp;
+	register char *	tp;
+	register int	c;
+	register char *	result;
+	char		dummy;
+
+	result = "";
+	if (string == NULL || format == NULL)
+		return result;
+	fbuf = imalloc(2 * strlen(format) + 4);
+	if (fbuf == NULL)
+		return result;
+	fp = format;
+	tp = fbuf;
+	while ((*tp++ = c = *fp++) != '\0') {
+		if (c != '%')
+			continue;
+		if (*fp == '%') {
+			*tp++ = *fp++;
+			continue;
+		}
+		*tp++ = '*';
+		if (*fp == '*')
+			++fp;
+		while (isascii(*fp) && isdigit(*fp))
+			*tp++ = *fp++;
+		if (*fp == 'l' || *fp == 'h')
+			*tp++ = *fp++;
+		else if (*fp == '[')
+			do *tp++ = *fp++;
+				while (*fp != '\0' && *fp != ']');
+		if ((*tp++ = *fp++) == '\0')
+			break;
+	}
+	*(tp - 1) = '%';
+	*tp++ = 'c';
+	*tp = '\0';
+	if (sscanf(string, fbuf, &dummy) != 1)
+		result = format;
+	free(fbuf);
+	return result;
+}
