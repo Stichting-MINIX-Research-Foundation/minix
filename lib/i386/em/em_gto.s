7dd225ffb2b75539631de26661748093552246e4
Ben Gras <ben@minix3.org>
2007-04-12 16:30:59 +0000

    delete redundant source

--- lib/i386/em/em_gto.s
+++
@@ -1,8 +0,0 @@
-.sect .text; .sect .rom; .sect .data; .sect .bss
-.sect .text
-.define .gto
-
-.gto:
-	mov     ebp,8(ebx)
-	mov     esp,4(ebx)
-	jmp     (ebx)
--- lib/i386/em/em_mon.s
+++
@@ -1,7 +0,0 @@
-.sect .text; .sect .rom; .sect .data; .sect .bss
-.sect .text
-.define .mon
-
-.mon:
-.extern .stop
-	call    .stop
