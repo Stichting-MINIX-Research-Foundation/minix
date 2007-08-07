4d2f56daf68e1765433815ba51b51c5982ab80f5
Philip Homburg <philip@cs.vu.nl>
2007-08-07 11:59:02 +0000

    Added prototype for mapdriver5 (from RS to VFS, report the name of a driver instead of its endpoint).

--- include/unistd.h
+++ include/unistd.h
@@ -198,6 +198,8 @@ _PROTOTYPE( int freemem, (phys_bytes size, phys_bytes base)		);
 #define unmapdriver(device) devctl(DEV_UNMAP, 0, device, 0)
 _PROTOTYPE( int devctl, (int ctl_req, int driver, int device, int style, 
 	int force)							);
+_PROTOTYPE( int mapdriver5, (char *label, size_t len, int major,
+	int style, int force)						);
 _PROTOTYPE( uid_t getpeuid, (endpoint_t ep)				);
 
 /* For compatibility with other Unix systems */
