2519a7cb61d5062e1093515d97e14c99a335cb58
Philip Homburg <philip@cs.vu.nl>
2007-08-07 12:14:04 +0000

    Added getuptime2.c

---
+++ lib/sysutil/getuptime2.c
@@ -0,0 +1,24 @@
+#include "sysutil.h"
+
+/*===========================================================================*
+ *                               getuptime2			    	     *
+ *===========================================================================*/
+PUBLIC int getuptime2(ticks, boottime)
+clock_t *ticks;				/* uptime in ticks */
+time_t *boottime;
+{
+    message m;
+    int s;
+
+    m.m_type = SYS_TIMES;		/* request time information */
+    m.T_ENDPT = NONE;			/* ignore process times */
+    s = _taskcall(SYSTASK, SYS_TIMES, &m);
+    *ticks = m.T_BOOT_TICKS;
+    *boottime = m.T_BOOTTIME;
+    return(s);
+}
+
+
+
+
+
