fe3e0181d4467afaad37bac44556b9b69a44edcf
Ben Gras <ben@minix3.org>
2008-12-11 14:55:06 +0000

    straggler.

---
+++ lib/sysutil/ser_putc.c
@@ -0,0 +1,27 @@
+#include "sysutil.h"
+#include <timers.h>
+
+#define COM1_BASE       0x3F8
+#define COM1_THR        (COM1_BASE + 0)
+#define         LSR_THRE        0x20
+#define COM1_LSR        (COM1_BASE + 5)
+
+/*===========================================================================*
+ *                               ser_putc			    	     *
+ *===========================================================================*/
+PUBLIC int ser_putc(char c)
+{
+        unsigned long b;
+        int i;
+        int lsr, thr;
+  
+        lsr= COM1_LSR;
+        thr= COM1_THR;
+        for (i= 0; i<10000; i++)
+        {
+                sys_inb(lsr, &b);
+                if (b & LSR_THRE)
+                        break;
+        }
+        sys_outb(thr, c);
+}
