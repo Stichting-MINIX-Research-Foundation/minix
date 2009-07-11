cf89d272186874be8f47f765349e37689bafd909
David van Moolenbroek <david@minix3.org>
2009-07-11 17:55:24 +0000

    make unstack work on gcc-compiled binaries

--- commands/scripts/unstack.sh
+++ commands/scripts/unstack.sh
@@ -5,12 +5,20 @@ then	echo "Usage: $0 <executable> [0x... [0x... ] ]"
 	exit 1
 fi
 
+PATH=$PATH:/usr/gnu/bin
+
+if file $1 | grep NSYM >/dev/null 2>&1; then
+  NM="gnm --radix=d"
+else
+  NM="nm -d"
+fi
+
 executable=$1
 shift
 
 while [ $# -gt 0 ]
 do	dec="`printf %d $1`"
-	nm -d -n $executable | grep ' [Tt] ' | awk '
+	$NM -n $executable | grep ' [Tt] ' | awk '
 	  {  if($1 > '$dec') { printf "%s+0x%x\n", name, '$dec'-offset; exit }
 	     name=$3; offset=$1
 	  }'
