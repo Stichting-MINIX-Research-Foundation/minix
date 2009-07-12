1d0e43c8f1fd0a5676396ad5c5407fa4e375cd0c
David van Moolenbroek <david@minix3.org>
2009-07-12 14:44:37 +0000

    make remove(3) remove directories as well

--- lib/stdio/remove.c
+++ lib/stdio/remove.c
@@ -4,10 +4,24 @@
 /* $Header$ */
 
 #include	<stdio.h>
+#include	<errno.h>
 
+int _rmdir(const char *path);
 int _unlink(const char *path);
 
 int
 remove(const char *filename) {
-	return _unlink(filename);
+	int saved_errno, retval;
+
+	saved_errno = errno;
+
+	retval = _rmdir(filename);
+
+	if (retval == -1 && errno == ENOTDIR) {
+		errno = saved_errno;
+
+		retval = _unlink(filename);
+	}
+
+	return retval;
 }
