d81563a9b12fa8d2a1127abd930bcea9dc817797
Erik van der Kouwe <erik@minix3.org>
2009-08-18 19:10:20 +0000

    Fix HUGE_VAL warnings

--- lib/math/hugeval.c
+++ lib/math/hugeval.c
@@ -5,10 +5,19 @@
  * Author: Hans van Eck
  */
 /* $Header$ */
+#include	<assert.h>
 #include	<math.h>
 
 double
 __huge_val(void)
 {
+#if (CHIP == INTEL)
+	static unsigned char ieee_infinity[] = { 
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x7f };
+
+	assert(sizeof(double) == sizeof(ieee_infinity));
+	return *(double *) ieee_infinity;
+#else
 	return 1.0e+1000;	/* This will generate a warning */
+#endif
 }
