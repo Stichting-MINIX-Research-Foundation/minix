2f4ea30d964660dbdc760027a28e23c5a2ef4409
Ben Gras <ben@minix3.org>
2009-10-05 16:40:21 +0000

    fix compiler warning

--- lib/syslib/vm_allocmem.c
+++ lib/syslib/vm_allocmem.c
@@ -11,7 +11,7 @@ PUBLIC int vm_allocmem(phys_clicks bytes, phys_clicks *retmembase)
     message m;
     int result;
 
-    m.VMAM_BYTES = bytes;
+    m.VMAM_BYTES = (char *) bytes;
     result = _taskcall(VM_PROC_NR, VM_ALLOCMEM, &m);
     if(result == OK)
 	    *retmembase = m.VMAM_MEMBASE;
