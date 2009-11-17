c85bd7edb92e116968f4841ed792f6c29837575c
Erik van der Kouwe <erik@minix3.org>
2009-11-17 08:41:43 +0000

    Patch by Jaswinder Singh Rajput to fix FTP progress display

--- commands/ftp/file.c
+++ commands/ftp/file.c
@@ -183,7 +183,7 @@ unsigned long total=0L;
 		}
 	}
 	if(atty && fd > 2) {
-		printf("%9lu bytes\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", total);
+		printf("%11lu bytes\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", total);
 		fflush(stdout);
 	}
    }
@@ -213,7 +213,7 @@ unsigned long total=0L;
 	write(fd, buffer, s);
 	total += (long)s;
 	if(atty && fd > 2) {
-		printf("%9lu bytes\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", total);
+		printf("%11lu bytes\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", total);
 		fflush(stdout);
 	}
    }
