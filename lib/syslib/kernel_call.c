d845891a50cd21e7c491cc886788a272b69f8fab
Tomas Hruby <tom@minix3.org>
2010-02-09 15:36:29 +0000

    And of course, as much as I've tried to be careful I forgot to add this file in r6116 :(

---
+++ lib/syslib/kernel_call.c
@@ -0,0 +1,11 @@
+#include <lib.h>
+#include <minix/syslib.h>
+
+PUBLIC int _kernel_call(int syscallnr, message *msgptr)
+{
+  int status;
+
+  msgptr->m_type = syscallnr;
+  _do_kernel_call(msgptr);
+  return(msgptr->m_type);
+}
