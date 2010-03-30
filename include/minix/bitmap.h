63e2d73d1b059e95c3b1a73194a2596787d3122f
Tomas Hruby <tom@minix3.org>
2010-03-30 08:34:33 +0000

    Fixed brackets in bitmap macros

--- include/minix/bitmap.h
+++ include/minix/bitmap.h
@@ -12,9 +12,9 @@
 #define BITCHUNK_BITS   (sizeof(bitchunk_t) * CHAR_BIT)
 #define BITMAP_CHUNKS(nr_bits) (((nr_bits)+BITCHUNK_BITS-1)/BITCHUNK_BITS)
 #define MAP_CHUNK(map,bit) (map)[((bit)/BITCHUNK_BITS)]
-#define CHUNK_OFFSET(bit) ((bit)%BITCHUNK_BITS))
-#define GET_BIT(map,bit) ( MAP_CHUNK(map,bit) & (1 << CHUNK_OFFSET(bit) )
-#define SET_BIT(map,bit) ( MAP_CHUNK(map,bit) |= (1 << CHUNK_OFFSET(bit) )
-#define UNSET_BIT(map,bit) ( MAP_CHUNK(map,bit) &= ~(1 << CHUNK_OFFSET(bit) )
+#define CHUNK_OFFSET(bit) ((bit)%BITCHUNK_BITS)
+#define GET_BIT(map,bit) ( MAP_CHUNK(map,bit) & (1 << CHUNK_OFFSET(bit) ))
+#define SET_BIT(map,bit) ( MAP_CHUNK(map,bit) |= (1 << CHUNK_OFFSET(bit) ))
+#define UNSET_BIT(map,bit) ( MAP_CHUNK(map,bit) &= ~(1 << CHUNK_OFFSET(bit) ))
 
 #endif	/* _BITMAP_H */
