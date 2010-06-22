f50f1bf7d6531bebfa377ebf71c7ad35197ec5be
Ben Gras <ben@minix3.org>
2010-06-22 00:41:00 +0000

    original netbsd patch

---
+++ commands/patch/Makefile
@@ -0,0 +1,8 @@
+#	$NetBSD: Makefile,v 1.9 2008/09/19 18:33:34 joerg Exp $
+#	$OpenBSD: Makefile,v 1.4 2005/05/16 15:22:46 espie Exp $
+#	$DragonFly: src/usr.bin/patch/Makefile,v 1.8 2008/08/10 23:50:12 joerg Exp $
+
+PROG=	patch
+SRCS=	patch.c pch.c inp.c util.c backupfile.c mkpath.c
+
+.include <bsd.prog.mk>
---
+++ commands/patch/pathnames.h
@@ -0,0 +1,14 @@
+/*
+ * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
+ * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.2 2008/08/11 00:04:12 joerg Exp $
+ * $NetBSD: pathnames.h,v 1.1 2008/09/19 18:33:34 joerg Exp $
+ */
+
+/*
+ * Placed in the public domain by Todd C. Miller <Todd.Miller@courtesan.com>
+ * on July 29, 2003.
+ */
+
+#include <paths.h>
+
+#define	_PATH_ED		"/bin/ed"
