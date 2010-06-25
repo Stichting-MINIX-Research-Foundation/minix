373aecf3cd1a830e2c2efb6030d7a3bdead4c67a
Ben Gras <ben@minix3.org>
2010-06-25 17:14:56 +0000

    commands: id from netbsd; touch from bsd-utils port

---
+++ commands/id/id.c
@@ -0,0 +1,364 @@
+/*-
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+
+#include <sys/param.h>
+
+#include <err.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void current(void);
+static void pretty(struct passwd *);
+static void group(struct passwd *, int);
+static void usage(void);
+static void user(struct passwd *);
+static struct passwd *who(char *);
+
+static int maxgroups;
+static gid_t *groups;
+
+int
+main(int argc, char *argv[])
+{
+	struct group *gr;
+	struct passwd *pw;
+	int ch, id;
+	int Gflag, gflag, nflag, pflag, rflag, uflag;
+	const char *opts;
+
+	Gflag = gflag = nflag = pflag = rflag = uflag = 0;
+
+	if (strcmp(getprogname(), "groups") == 0) {
+		Gflag = 1;
+		nflag = 1;
+		opts = "";
+		if (argc > 2)
+			usage();
+	} else if (strcmp(getprogname(), "whoami") == 0) {
+		uflag = 1;
+		nflag = 1;
+		opts = "";
+		if (argc > 1)
+			usage();
+	} else
+		opts = "Ggnpru";
+
+	while ((ch = getopt(argc, argv, opts)) != -1)
+		switch (ch) {
+		case 'G':
+			Gflag = 1;
+			break;
+		case 'g':
+			gflag = 1;
+			break;
+		case 'n':
+			nflag = 1;
+			break;
+		case 'p':
+			pflag = 1;
+			break;
+		case 'r':
+			rflag = 1;
+			break;
+		case 'u':
+			uflag = 1;
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	switch (Gflag + gflag + pflag + uflag) {
+	case 1:
+		break;
+	case 0:
+		if (!nflag && !rflag)
+			break;
+		/* FALLTHROUGH */
+	default:
+		usage();
+	}
+
+	if (strcmp(opts, "") != 0 && argc > 1)
+		usage();
+
+	pw = *argv ? who(*argv) : NULL;
+
+	maxgroups = sysconf(_SC_NGROUPS_MAX);
+	if ((groups = malloc((maxgroups + 1) * sizeof(gid_t))) == NULL)
+		err(1, NULL);
+
+	if (gflag) {
+		id = pw ? pw->pw_gid : rflag ? getgid() : getegid();
+		if (nflag && (gr = getgrgid(id)))
+			(void)printf("%s\n", gr->gr_name);
+		else
+			(void)printf("%u\n", id);
+		goto done;
+	}
+
+	if (uflag) {
+		id = pw ? pw->pw_uid : rflag ? getuid() : geteuid();
+		if (nflag && (pw = getpwuid(id)))
+			(void)printf("%s\n", pw->pw_name);
+		else
+			(void)printf("%u\n", id);
+		goto done;
+	}
+
+	if (Gflag) {
+		group(pw, nflag);
+		goto done;
+	}
+
+	if (pflag) {
+		pretty(pw);
+		goto done;
+	}
+
+	if (pw)
+		user(pw);
+	else
+		current();
+done:
+	free(groups);
+
+	return 0;
+}
+
+static void
+pretty(struct passwd *pw)
+{
+	struct group *gr;
+	u_int eid, rid;
+	char *login;
+
+	if (pw) {
+		(void)printf("uid\t%s\n", pw->pw_name);
+		(void)printf("groups\t");
+		group(pw, 1);
+	} else {
+		if ((login = getlogin()) == NULL)
+			err(1, "getlogin");
+
+		pw = getpwuid(rid = getuid());
+		if (pw == NULL || strcmp(login, pw->pw_name))
+			(void)printf("login\t%s\n", login);
+		if (pw)
+			(void)printf("uid\t%s\n", pw->pw_name);
+		else
+			(void)printf("uid\t%u\n", rid);
+
+		if ((eid = geteuid()) != rid) {
+			if ((pw = getpwuid(eid)) != NULL)
+				(void)printf("euid\t%s\n", pw->pw_name);
+			else
+				(void)printf("euid\t%u\n", eid);
+		}
+		if ((rid = getgid()) != (eid = getegid())) {
+			if ((gr = getgrgid(rid)) != NULL)
+				(void)printf("rgid\t%s\n", gr->gr_name);
+			else
+				(void)printf("rgid\t%u\n", rid);
+		}
+		(void)printf("groups\t");
+		group(NULL, 1);
+	}
+}
+
+static void
+current(void)
+{
+	struct group *gr;
+	struct passwd *pw;
+	gid_t gid, egid, lastid;
+	uid_t uid, euid;
+	int cnt, ngroups;
+	const char *fmt;
+
+	uid = getuid();
+	(void)printf("uid=%ju", (uintmax_t)uid);
+	if ((pw = getpwuid(uid)) != NULL)
+		(void)printf("(%s)", pw->pw_name);
+	gid = getgid();
+	(void)printf(" gid=%ju", (uintmax_t)gid);
+	if ((gr = getgrgid(gid)) != NULL)
+		(void)printf("(%s)", gr->gr_name);
+	if ((euid = geteuid()) != uid) {
+		(void)printf(" euid=%ju", (uintmax_t)euid);
+		if ((pw = getpwuid(euid)) != NULL)
+			(void)printf("(%s)", pw->pw_name);
+	}
+	if ((egid = getegid()) != gid) {
+		(void)printf(" egid=%ju", (uintmax_t)egid);
+		if ((gr = getgrgid(egid)) != NULL)
+			(void)printf("(%s)", gr->gr_name);
+	}
+#ifndef __minix
+	if ((ngroups = getgroups(maxgroups, groups)) != 0) {
+		for (fmt = " groups=%ju", lastid = -1, cnt = 0; cnt < ngroups;
+		    fmt = ",%ju", lastid = gid, cnt++) {
+			gid = groups[cnt];
+			if (lastid == gid)
+				continue;
+			(void)printf(fmt, (uintmax_t)gid);
+			if ((gr = getgrgid(gid)) != NULL)
+				(void)printf("(%s)", gr->gr_name);
+		}
+	}
+#endif
+	(void)printf("\n");
+}
+
+static void
+user(struct passwd *pw)
+{
+	struct group *gr;
+	const char *fmt;
+	int cnt, id, lastid, ngroups;
+	gid_t *glist = groups;
+
+	id = pw->pw_uid;
+	(void)printf("uid=%u(%s)", id, pw->pw_name);
+	(void)printf(" gid=%lu", (u_long)pw->pw_gid);
+	if ((gr = getgrgid(pw->pw_gid)) != NULL)
+		(void)printf("(%s)", gr->gr_name);
+	ngroups = maxgroups + 1;
+#ifndef __minix
+	if (getgrouplist(pw->pw_name, pw->pw_gid, glist, &ngroups) == -1) {
+		glist = malloc(ngroups * sizeof(gid_t));
+		(void) getgrouplist(pw->pw_name, pw->pw_gid, glist, &ngroups);
+	}
+#endif
+	for (fmt = " groups=%u", lastid = -1, cnt = 0; cnt < ngroups;
+	    fmt=",%u", lastid = id, cnt++) {
+		id = glist[cnt];
+		if (lastid == id)
+			continue;
+		(void)printf(fmt, id);
+		if ((gr = getgrgid(id)) != NULL)
+			(void)printf("(%s)", gr->gr_name);
+	}
+	(void)printf("\n");
+	if (glist != groups)
+		free(glist);
+}
+
+static void
+group(struct passwd *pw, int nflag)
+{
+	struct group *gr;
+	int cnt, id, lastid, ngroups;
+	const char *fmt;
+	gid_t *glist = groups;
+
+	if (pw) {
+		ngroups = maxgroups;
+#ifndef __minix
+		if (getgrouplist(pw->pw_name, pw->pw_gid, glist, &ngroups)
+		    == -1) {
+			glist = malloc(ngroups * sizeof(gid_t));
+			(void) getgrouplist(pw->pw_name, pw->pw_gid, glist,
+					    &ngroups);
+
+		}
+#endif
+	} else {
+		glist[0] = getgid();
+		ngroups = getgroups(maxgroups, glist + 1) + 1;
+	}
+	fmt = nflag ? "%s" : "%u";
+	for (lastid = -1, cnt = 0; cnt < ngroups; ++cnt) {
+		if (lastid == (id = glist[cnt]) || (cnt && id == glist[0]))
+			continue;
+		if (nflag) {
+			if ((gr = getgrgid(id)) != NULL)
+				(void)printf(fmt, gr->gr_name);
+			else
+				(void)printf(*fmt == ' ' ? " %u" : "%u",
+				    id);
+			fmt = " %s";
+		} else {
+			(void)printf(fmt, id);
+			fmt = " %u";
+		}
+		lastid = id;
+	}
+	(void)printf("\n");
+	if (glist != groups)
+		free(glist);
+}
+
+static struct passwd *
+who(char *u)
+{
+	struct passwd *pw;
+	long id;
+	char *ep;
+
+	/*
+	 * Translate user argument into a pw pointer.  First, try to
+	 * get it as specified.  If that fails, try it as a number.
+	 */
+	if ((pw = getpwnam(u)) != NULL)
+		return pw;
+	id = strtol(u, &ep, 10);
+	if (*u && !*ep && (pw = getpwuid(id)))
+		return pw;
+	errx(1, "%s: No such user", u);
+	/* NOTREACHED */
+	return NULL;
+}
+
+static void
+usage(void)
+{
+
+	if (strcmp(getprogname(), "groups") == 0) {
+		(void)fprintf(stderr, "usage: groups [user]\n");
+	} else if (strcmp(getprogname(), "whoami") == 0) {
+		(void)fprintf(stderr, "usage: whoami\n");
+	} else {
+		(void)fprintf(stderr, "usage: id [user]\n");
+		(void)fprintf(stderr, "       id -G [-n] [user]\n");
+		(void)fprintf(stderr, "       id -g [-nr] [user]\n");
+		(void)fprintf(stderr, "       id -p [user]\n");
+		(void)fprintf(stderr, "       id -u [-nr] [user]\n");
+	}
+	exit(1);
+}
---
+++ commands/touch/touch.c
@@ -0,0 +1,331 @@
+/*	$OpenBSD: touch.c,v 1.17 2007/08/06 19:16:06 sobrado Exp $	*/
+/*	$NetBSD: touch.c,v 1.11 1995/08/31 22:10:06 jtc Exp $	*/
+
+/*
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <time.h>
+#include <utime.h>
+#include <tzfile.h>
+#include <unistd.h>
+
+void		stime_arg1(char *, struct timeval *);
+void		stime_arg2(char *, int, struct timeval *);
+void		stime_file(char *, struct timeval *);
+__dead void	usage(void);
+
+char * __progname;
+
+int
+main(int argc, char *argv[])
+{
+	struct stat	 sb;
+	struct timeval	 tv[2];
+	struct utimbuf	 timbuf;
+	int		 aflag, cflag, mflag, ch, fd, len, rval, timeset;
+	char		*p;
+
+	(void)setlocale(LC_ALL, "");
+	__progname = argv[0];
+
+	aflag = cflag = mflag = timeset = 0;
+	if (gettimeofday(&tv[0], NULL))
+		err(1, "gettimeofday");
+
+	while ((ch = getopt(argc, argv, "acfmr:t:")) != -1)
+		switch (ch) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'c':
+			cflag = 1;
+			break;
+		case 'f':
+			break;
+		case 'm':
+			mflag = 1;
+			break;
+		case 'r':
+			timeset = 1;
+			stime_file(optarg, tv);
+			break;
+		case 't':
+			timeset = 1;
+			stime_arg1(optarg, tv);
+			break;
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	/* Default is both -a and -m. */
+	if (aflag == 0 && mflag == 0)
+		aflag = mflag = 1;
+
+	/*
+	 * If no -r or -t flag, at least two operands, the first of which
+	 * is an 8 or 10 digit number, use the obsolete time specification.
+	 */
+	if (!timeset && argc > 1) {
+		(void)strtol(argv[0], &p, 10);
+		len = p - argv[0];
+		if (*p == '\0' && (len == 8 || len == 10)) {
+			timeset = 1;
+			stime_arg2(*argv++, len == 10, tv);
+		}
+	}
+
+	/* Otherwise use the current time of day. */
+	if (!timeset)
+		tv[1] = tv[0];
+
+	if (*argv == NULL)
+		usage();
+
+	for (rval = 0; *argv; ++argv) {
+		/* See if the file exists. */
+		if (stat(*argv, &sb)) {
+			if (!cflag) {
+				/* Create the file. */
+				fd = open(*argv,
+				    O_WRONLY | O_CREAT, DEFFILEMODE);
+				if (fd == -1 || fstat(fd, &sb) || close(fd)) {
+					rval = 1;
+					warn("%s", *argv);
+					continue;
+				}
+
+				/* If using the current time, we're done. */
+				if (!timeset)
+					continue;
+			} else
+				continue;
+		}
+
+#if 0
+		if (!aflag)
+			TIMESPEC_TO_TIMEVAL(&tv[0], &sb.st_atimespec);
+		if (!mflag)
+			TIMESPEC_TO_TIMEVAL(&tv[1], &sb.st_mtimespec);
+#else
+		if (!aflag)
+			tv[0].tv_sec = sb.st_atime;
+		if (!mflag)
+			tv[1].tv_sec = sb.st_mtime;
+#endif
+
+		/* Try utimes(2). */
+#if 0
+		if (!utimes(*argv, tv))
+			continue;
+#else
+		timbuf.actime = tv[0].tv_sec;
+		timbuf.modtime = tv[1].tv_sec;
+		if (!utime(*argv, &timbuf))
+			continue;
+#endif
+
+		/* If the user specified a time, nothing else we can do. */
+		if (timeset) {
+			rval = 1;
+			warn("%s", *argv);
+		}
+
+		/*
+		 * System V and POSIX 1003.1 require that a NULL argument
+		 * set the access/modification times to the current time.
+		 * The permission checks are different, too, in that the
+		 * ability to write the file is sufficient.  Take a shot.
+		 */
+#if 0
+		 if (!utimes(*argv, NULL))
+			continue;
+#else
+		if (!utime(*argv, NULL))
+			continue;
+#endif
+
+		rval = 1;
+		warn("%s", *argv);
+	}
+	exit(rval);
+}
+
+#define	ATOI2(s)	((s) += 2, ((s)[-2] - '0') * 10 + ((s)[-1] - '0'))
+
+void
+stime_arg1(char *arg, struct timeval *tvp)
+{
+	struct tm	*lt;
+	time_t		 tmptime;
+	int		 yearset;
+	char		*dot, *p;
+					/* Start with the current time. */
+	tmptime = tvp[0].tv_sec;
+	if ((lt = localtime(&tmptime)) == NULL)
+		err(1, "localtime");
+					/* [[CC]YY]MMDDhhmm[.SS] */
+	for (p = arg, dot = NULL; *p != '\0'; p++) {
+		if (*p == '.' && dot == NULL)
+			dot = p;
+		else if (!isdigit((unsigned char)*p))
+			goto terr;
+	}
+	if (dot == NULL)
+		lt->tm_sec = 0;		/* Seconds defaults to 0. */
+	else {
+		*dot++ = '\0';
+		if (strlen(dot) != 2)
+			goto terr;
+		lt->tm_sec = ATOI2(dot);
+		if (lt->tm_sec > 61)	/* Could be leap second. */
+			goto terr;
+	}
+
+	yearset = 0;
+	switch (strlen(arg)) {
+	case 12:			/* CCYYMMDDhhmm */
+		lt->tm_year = ATOI2(arg) * 100 - TM_YEAR_BASE;
+		yearset = 1;
+		/* FALLTHROUGH */
+	case 10:			/* YYMMDDhhmm */
+		if (yearset) {
+			yearset = ATOI2(arg);
+			lt->tm_year += yearset;
+		} else {
+			yearset = ATOI2(arg);
+			/* Preserve current century. */
+			lt->tm_year = ((lt->tm_year / 100) * 100) + yearset;
+		}
+		/* FALLTHROUGH */
+	case 8:				/* MMDDhhmm */
+		lt->tm_mon = ATOI2(arg);
+		if (lt->tm_mon > 12 || lt->tm_mon == 0)
+			goto terr;
+		--lt->tm_mon;		/* Convert from 01-12 to 00-11 */
+		lt->tm_mday = ATOI2(arg);
+		if (lt->tm_mday > 31 || lt->tm_mday == 0)
+			goto terr;
+		lt->tm_hour = ATOI2(arg);
+		if (lt->tm_hour > 23)
+			goto terr;
+		lt->tm_min = ATOI2(arg);
+		if (lt->tm_min > 59)
+			goto terr;
+		break;
+	default:
+		goto terr;
+	}
+
+	lt->tm_isdst = -1;		/* Figure out DST. */
+	tvp[0].tv_sec = tvp[1].tv_sec = mktime(lt);
+	if (tvp[0].tv_sec == -1)
+terr:		errx(1,
+	"out of range or illegal time specification: [[CC]YY]MMDDhhmm[.SS]");
+
+	tvp[0].tv_usec = tvp[1].tv_usec = 0;
+}
+
+void
+stime_arg2(char *arg, int year, struct timeval *tvp)
+{
+	struct tm	*lt;
+	time_t		 tmptime;
+					/* Start with the current time. */
+	tmptime = tvp[0].tv_sec;
+	if ((lt = localtime(&tmptime)) == NULL)
+		err(1, "localtime");
+
+	lt->tm_mon = ATOI2(arg);	/* MMDDhhmm[YY] */
+	if (lt->tm_mon > 12 || lt->tm_mon == 0)
+		goto terr;
+	--lt->tm_mon;			/* Convert from 01-12 to 00-11 */
+	lt->tm_mday = ATOI2(arg);
+	if (lt->tm_mday > 31 || lt->tm_mday == 0)
+		goto terr;
+	lt->tm_hour = ATOI2(arg);
+	if (lt->tm_hour > 23)
+		goto terr;
+	lt->tm_min = ATOI2(arg);
+	if (lt->tm_min > 59)
+		goto terr;
+	if (year) {
+		year = ATOI2(arg);	/* Preserve current century. */
+		lt->tm_year = ((lt->tm_year / 100) * 100) + year;
+	}
+	lt->tm_sec = 0;
+
+	lt->tm_isdst = -1;		/* Figure out DST. */
+	tvp[0].tv_sec = tvp[1].tv_sec = mktime(lt);
+	if (tvp[0].tv_sec == -1)
+terr:		errx(1,
+	"out of range or illegal time specification: MMDDhhmm[YY]");
+
+	tvp[0].tv_usec = tvp[1].tv_usec = 0;
+}
+
+void
+stime_file(char *fname, struct timeval *tvp)
+{
+	struct stat	sb;
+
+	if (stat(fname, &sb))
+		err(1, "%s", fname);
+#if 0
+	TIMESPEC_TO_TIMEVAL(tvp, &sb.st_atimespec);
+	TIMESPEC_TO_TIMEVAL(tvp + 1, &sb.st_mtimespec);
+#else
+	(tvp + 0)->tv_sec = sb.st_atime;
+	(tvp + 1)->tv_sec = sb.st_mtime;
+#endif
+}
+
+__dead void
+usage(void)
+{
+	extern char	*__progname;
+
+	(void)fprintf(stderr,
+	    "usage: %s [-acm] [-r file] [-t [[CC]YY]MMDDhhmm[.SS]] file ...\n",
+	    __progname);
+	exit(1);
+}
