53b6f99fb026308615a5146f2d0bdd140aed74d9
Ben Gras <ben@minix3.org>
2010-07-19 11:39:57 +0000

    includes: remove some prototypes of functions that aren't defined.

--- include/wctype.h
+++ include/wctype.h
@@ -62,23 +62,11 @@ typedef	void *wctrans_t;
 #define	WEOF	((wint_t)-1)
 #endif
 
-int	iswalnum(wint_t);
-int	iswalpha(wint_t);
-int	iswblank(wint_t);
-int	iswcntrl(wint_t);
-int	iswdigit(wint_t);
-int	iswgraph(wint_t);
-int	iswlower(wint_t);
-int	iswprint(wint_t);
-int	iswpunct(wint_t);
-int	iswspace(wint_t);
-int	iswupper(wint_t);
-int	iswxdigit(wint_t);
 int	iswctype(wint_t, wctype_t);
 wint_t	towctrans(wint_t, wctrans_t);
-wint_t	towlower(wint_t);
-wint_t	towupper(wint_t);
 wctrans_t wctrans(const char *);
 wctype_t wctype(const char *);
+wint_t towlower(wint_t);
+wint_t towupper(wint_t);
 
 #endif		/* _WCTYPE_H_ */
