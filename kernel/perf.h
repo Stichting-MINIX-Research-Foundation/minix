4ee139b0beeb4401c3b7436c1a65f52dc7523d83
Tomas Hruby <tom@minix3.org>
2010-09-15 14:11:30 +0000

    SMP - all process have pagetables
    - all processes have private pagetables if CONFIG_SMP is set
    
    - this make possible to safely schedule PM, RS, VFS anywhere

--- kernel/perf.h
+++ kernel/perf.h
@@ -7,6 +7,10 @@
 #define PERF_USE_COW_SAFECOPY 0
 
 /* Use a private page table for critical system processes. */
+#ifdef CONFIG_SMP
+#define PERF_SYS_CORE_FULLVM 1
+#else
 #define PERF_SYS_CORE_FULLVM 0
+#endif
 
 #endif /* PERF_H */
