467a01024e57d5cc9018463cbbd36882c196d8b7
Ben Gras <ben@minix3.org>
2010-09-15 08:47:42 +0000

    bump trunk version to 3.1.9
    make issue.install not include version

--- include/minix/config.h
+++ include/minix/config.h
@@ -3,7 +3,7 @@
 
 /* Minix release and version numbers. */
 #define OS_RELEASE "3"
-#define OS_VERSION "1.8"
+#define OS_VERSION "1.9"
 
 /* This file sets configuration parameters for the MINIX kernel, FS, and PM.
  * It is divided up into two main sections.  The first section contains
