d0d7b4334294e032d433c1f5c18b9e9982e4cb1c
Ben Gras <ben@minix3.org>
2011-06-28 17:17:44 +0200

    minix.libc.mk: simpler netbsd libc invocation
      . startup files being mentioned explicitly confuses libtool

--- share/mk/minix.libc.mk
+++ share/mk/minix.libc.mk
@@ -1,7 +1,6 @@
 # NetBSD libc flags go here
-NETBSD_LIBC_CPPFLAGS=-nostdinc -D__NBSD_LIBC -isystem /usr/netbsd/include
-NETBSD_LIBC_LDFLAGS=-nostdlib /usr/netbsd/lib/crt1.o /usr/netbsd/lib/crti.o /usr/netbsd/lib/crtn.o -L/usr/netbsd/lib -lc -L/usr/pkg/lib -lgcc -L/usr/netbsd/lib -lminlib -lcompat_minix
-
+NETBSD_LIBC_CPPFLAGS=-B/usr/netbsd -D__NBSD_LIBC
+NETBSD_LIBC_LDFLAGS=-B/usr/netbsd:/usr/netbsd/lib -lminlib -lcompat_minix
 # Minix libc flags go here
 MINIX_LIBC_CPPFLAGS=
 MINIX_LIBC_LDFLAGS=
