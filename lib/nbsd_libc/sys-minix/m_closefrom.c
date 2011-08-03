a99a4b2a056bec8e037f4e10252247b6905a8781
Ben Gras <ben@minix3.org>
2011-08-03 12:51:28 +0200

    added missing m_closefrom.c

---
+++ lib/nbsd_libc/sys-minix/m_closefrom.c
@@ -0,0 +1,23 @@
+#include <sys/cdefs.h>
+#include "namespace.h"
+#include <lib.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+PUBLIC int closefrom(int fd)
+{
+	int f, ok = 0, e;
+	for(f = fd; f < __MINIX_OPEN_MAX; f++) {
+		if(close(f) >= 0)
+			ok = 1;
+		else
+			e = errno;
+	}
+
+	if(ok)
+		return 0;
+
+	/* all failed - return last valid error */
+	errno = e;
+	return -1;
+}
