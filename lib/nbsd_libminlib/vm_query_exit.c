7b70f548e446c739c6d451773cc8377a7e732184
Arun Thomas <arun@minix3.org>
2011-08-12 17:05:08 +0200

    minlib: Add vm_query_exit

--- lib/nbsd_libminlib/vm_query_exit.c
+++ lib/nbsd_libminlib/vm_query_exit.c
@@ -21,3 +21,13 @@ PUBLIC int vm_query_exit(int *endpt)
 	*endpt = m.VM_QUERY_RET_PT;
 	return (m.VM_QUERY_IS_MORE ? 1 : 0);
 }
+
+PUBLIC int vm_watch_exit(endpoint_t ep)
+{
+	message m;
+	int r;
+
+	memset(&m, 0, sizeof(m));
+	m.VM_WE_EP = ep;
+	return _syscall(VM_PROC_NR, VM_WATCH_EXIT, &m);
+}
