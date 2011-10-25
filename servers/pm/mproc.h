b1c23299541ac1c056c9ed6b5994f8fbdc3ea539
Antoine LECA <Antoine.Leca.1@gmail.com>
2011-10-25 16:36:54 +0000

    clean up PM_SCHEDULED

--- servers/pm/mproc.h
+++ servers/pm/mproc.h
@@ -84,6 +84,5 @@ EXTERN struct mproc {
 #define TRACE_EXIT	0x08000	/* tracer is forcing this process to exit */
 #define TRACE_ZOMBIE	0x10000	/* waiting for tracer to issue WAIT call */
 #define DELAY_CALL	0x20000	/* waiting for call before sending signal */
-#define PM_SCHEDULED	0x40000 /* this process is scheduled by PM */
 
 
