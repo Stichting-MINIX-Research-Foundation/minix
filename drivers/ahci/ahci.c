Stichting-MINIX-Research-Foundation/minix

commit b19820774e03c366925928bac684b5c065d007ac

fixed clang warnings in drivers/
  . changed debug statements system for audio/ to do so

drivers/ahci/ahci.c
@@ -110,6 +110,7 @@
 #include <minix/drivers.h>
 #include <minix/driver.h>
 #include <minix/drvlib.h>
+#include <minix/u64.h>
 #include <machine/pci.h>
 #include <sys/ioc_disk.h>
 #include <sys/mman.h>

drivers/dp8390/rtl8029.c
@@ -142,7 +142,6 @@ static void rtl_init(dep)
 dpeth_t *dep;
 {
 	u8_t reg_a, reg_b, cr, config0, config2, config3;
-	int i;
 
 #if DEBUG
 	printf("rtl_init called\n");

drivers/fxp/fxp.c
@@ -60,7 +60,7 @@ PRIVATE struct pcitab pcitab_fxp[]=
 typedef int irq_hook_t;
 
 /* ignore interrupt for the moment */
-#define interrupt(x)	0
+#define interrupt(x)	do { } while(0)
 
 PRIVATE union tmpbuf
 {
@@ -421,7 +421,6 @@ message *mp;
 static void fxp_pci_conf()
 {
 	static char envvar[] = FXP_ENVVAR "#";
-	static char envfmt[] = "*:d.d.d";
 
 	fxp_t *fp;
 	long v;
@@ -627,7 +626,6 @@ static int fxp_probe(fxp_t *fp, int skip)
  *===========================================================================*/
 static void fxp_conf_hw(fxp_t *fp)
 {
-	int i;
 	int mwi, ext_stat1, ext_stat2, lim_fifo, i82503, fc;
 
 	fp->fxp_mode= FM_DISABLED;	/* Superfluous */

drivers/lance/lance.c
@@ -645,7 +645,7 @@ ec_conf_t *ecp;
 static void ec_init(ec)
 ether_card_t *ec;
 {
-   int i, r;
+   int r;
 
    /* General initialization */
    ec->flags = ECF_EMPTY;

drivers/rtl8139/rtl8139.c
@@ -461,7 +461,6 @@ int skip;
 	u16_t vid, did;
 	u32_t bar;
 	u8_t ilr;
-	char *dname;
 
 	if ((rep->re_pcibus | rep->re_pcidev | rep->re_pcifunc) != 0)
 	{
@@ -864,7 +863,6 @@ static void rl_readv_s(const message *mp, int from_int)
 	int i, j, n, o, s, s1, count, size;
 	port_t port;
 	unsigned amount, totlen, packlen;
-	phys_bytes dst_phys;
 	u16_t d_start, d_end;
 	u32_t l, rxstat = 0x12345678;
 	re_t *rep;

drivers/ti1225/ti1225.c
@@ -54,8 +54,6 @@ PRIVATE int debug;
 FORWARD _PROTOTYPE( void hw_init, (struct port *pp)			);
 FORWARD _PROTOTYPE( void map_regs, (struct port *pp, u32_t base)	);
 FORWARD _PROTOTYPE( void do_int, (struct port *pp)			);
-FORWARD _PROTOTYPE( void do_outb, (port_t port, u8_t value)		);
-FORWARD _PROTOTYPE( u8_t do_inb, (port_t port)				);
 
 /* SEF functions and variables. */
 FORWARD _PROTOTYPE( void sef_local_startup, (void) );
@@ -185,7 +183,7 @@ PRIVATE int sef_cb_init_fresh(int type, sef_init_info_t *info)
 
 PRIVATE void hw_init(struct port *pp)
 {
-	int r, devind, irq;
+	int devind;
 	u8_t v8;
 	u16_t v16;
 	u32_t v32;
@@ -312,7 +310,7 @@ PRIVATE void map_regs(struct port *pp, u32_t base)
 
 PRIVATE void do_int(struct port *pp)
 {
-	int r, devind, vcc_5v, vcc_3v, vcc_Xv, vcc_Yv,
+	int devind, vcc_5v, vcc_3v, vcc_Xv, vcc_Yv,
 		socket_5v, socket_3v, socket_Xv, socket_Yv;
 	spin_t spin;
 	u32_t csr_event, csr_present, csr_control;
@@ -491,26 +489,4 @@ PRIVATE void do_int(struct port *pp)
 #endif
 
 }
-
-
-PRIVATE u8_t do_inb(port_t port)
-{
-	int r;
-	u32_t value;
-
-	r= sys_inb(port, &value);
-	if (r != OK)
-		panic("sys_inb failed: %d", r);
-	return value;
-}
-
-PRIVATE void do_outb(port_t port, u8_t value)
-{
-	int r;
-
-	r= sys_outb(port, value);
-	if (r != OK)
-		panic("sys_outb failed: %d", r);
-}
-
 

common/include/minix/audio_fw.h
@@ -6,18 +6,6 @@
 #include <sys/ioc_sound.h>
 
 
-/* change to DEBUG to 1 to print debug info and error messages */
-
-#define DEBUG 0
-
-#if DEBUG
-#define dprint printf 
-#else
-#define dprint (void) 
-#endif
-#define error printf
-
-
 _PROTOTYPE( int drv_init, (void) );
 _PROTOTYPE( int drv_init_hw, (void) );
 _PROTOTYPE( int drv_reset, (void) );

drivers/audio/es1371/AC97.c
@@ -6,11 +6,8 @@
 
 /* AC97 Mixer and Mode control function prototypes */
 
-FORWARD _PROTOTYPE( int  AC97_read, 
-		(const DEV_STRUCT * pCC, u16_t wAddr, u16_t *data) );
 FORWARD _PROTOTYPE( int  AC97_write, 
 		(const DEV_STRUCT * pCC, u16_t wAddr, u16_t wData) );
-FORWARD _PROTOTYPE( void set_src_sync_state, (int state) );
 FORWARD _PROTOTYPE( int  AC97_write_unsynced, 
 		(const DEV_STRUCT * pCC, u16_t wAddr, u16_t wData) );
 FORWARD _PROTOTYPE( int  AC97_read_unsynced, 
@@ -58,6 +55,7 @@ static u32_t SrcSyncState = 0x00010000UL;
 static DEV_STRUCT *dev;
 
 
+#if 0
 PRIVATE void set_src_sync_state (int state)
 {
     if (state < 0)
@@ -67,6 +65,7 @@ PRIVATE void set_src_sync_state (int state)
         SrcSyncState &= 0x00070000Ul;
     }
 }
+#endif
 
 
 PRIVATE int AC97_write (const DEV_STRUCT * pCC, u16_t wAddr, u16_t wData)
@@ -133,6 +132,7 @@ u16_t  wBaseAddr = pCC->base;
 }
 
 
+#if 0
 PRIVATE int AC97_read (const DEV_STRUCT * pCC, u16_t wAddr, u16_t *data)
 {
 u32_t dtemp, i;
@@ -204,6 +204,7 @@ u16_t  base = pCC->base;
 
     return 0;
 }
+#endif
 
 
 PRIVATE int AC97_write_unsynced (const DEV_STRUCT * pCC, u16_t wAddr,

commit 47aad344c3b17bd1d6a6907b3b071807ab767428

Drivers: fix issues introduced by commit b198207

drivers/ahci/ahci.c
@@ -106,7 +106,6 @@
 #include <minix/drivers.h>
 #include <minix/driver_mt.h>
 #include <minix/drvlib.h>
-#include <minix/u64.h>
 #include <machine/pci.h>
 #include <sys/ioc_disk.h>
 #include <sys/mman.h>

drivers/dp8390/rtl8029.c
@@ -193,7 +193,7 @@ dpeth_t *dep;
 #endif
 	}
 
-#if DEBUG
+#if 0
 	for (i= 0; i<64; i++)
 		printf("%x ", get_ee_word(dep, i));
 	printf("\n");

drivers/fxp/fxp.c
@@ -632,6 +632,9 @@ static int fxp_probe(fxp_t *fp, int skip)
 static void fxp_conf_hw(fxp_t *fp)
 {
 	int mwi, ext_stat1, ext_stat2, lim_fifo, i82503, fc;
+#if VERBOSE
+	int i;
+#endif
 
 	fp->fxp_mode= FM_DISABLED;	/* Superfluous */
 

drivers/lance/lance.c
@@ -646,6 +646,9 @@ static void ec_init(ec)
 ether_card_t *ec;
 {
    int r;
+#if VERBOSE
+   int i;
+#endif
 
    /* General initialization */
    ec->flags = ECF_EMPTY;

drivers/rtl8139/rtl8139.c
@@ -461,6 +461,9 @@ int skip;
 	u16_t vid, did;
 	u32_t bar;
 	u8_t ilr;
+#if VERBOSE
+	char *dname;
+#endif
 
 	if ((rep->re_pcibus | rep->re_pcidev | rep->re_pcifunc) != 0)
 	{

drivers/ti1225/ti1225.c
@@ -187,6 +187,9 @@ PRIVATE void hw_init(struct port *pp)
 	u8_t v8;
 	u16_t v16;
 	u32_t v32;
+#if USE_INTS
+	int r, irq;
+#endif
 
 	devind= pp->p_devind;
 	if (debug)
@@ -316,6 +319,9 @@ PRIVATE void do_int(struct port *pp)
 	u32_t csr_event, csr_present, csr_control;
 	u8_t v8;
 	u16_t v16;
+#if USE_INTS
+	int r;
+#endif
 
 	devind= pp->p_devind;
 	v8= pci_attr_r8(devind, TI_CARD_CTRL);