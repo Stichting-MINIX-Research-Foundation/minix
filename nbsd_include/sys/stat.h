c4bce95897e7d69982156bd0d14e0a7988ff0cfd
Vivek Prakash <vivek.cs.iitr@gmail.com>
2011-11-13 04:38:52 +0000

    Define flags in sys/stat.h

--- nbsd_include/sys/stat.h
+++ nbsd_include/sys/stat.h
@@ -143,6 +143,25 @@ struct minix_prev_stat {
 
 #define S_BLKSIZE	512		/* block size used in the stat struct */
 
+/*
+ * Definitions of flags stored in file flags word.
+ *
+ * Super-user and owner changeable flags.
+ */
+#define	UF_SETTABLE	0x0000ffff	/* mask of owner changeable flags */
+#define	UF_NODUMP	0x00000001	/* do not dump file */
+#define	UF_IMMUTABLE	0x00000002	/* file may not be changed */
+#define	UF_APPEND	0x00000004	/* writes to file may only append */
+#define UF_OPAQUE	0x00000008	/* directory is opaque wrt. union */
+
+/*
+ * Super-user changeable flags.
+ */
+#define	SF_SETTABLE	0xffff0000	/* mask of superuser changeable flags */
+#define	SF_ARCHIVED	0x00010000	/* file is archived */
+#define	SF_IMMUTABLE	0x00020000	/* file may not be changed */
+#define	SF_APPEND	0x00040000	/* writes to file may only append */
+
 #endif /* _NETBSD_SOURCE */
 
 #if defined(__minix)
