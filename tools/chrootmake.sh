d4ddfeb2c0b2851ca3ed75ae0381feb6e9f67e06
Evgeniy Ivanov <lolkaantimat@gmail.com>
2012-02-06 17:56:49 +0400

    Build both old boot and new boot (for setup).

--- tools/chrootmake.sh
+++ tools/chrootmake.sh
@@ -8,13 +8,22 @@ then	make $@
 	exit $?
 fi
 
-CC=clang make world
+make world
+
 cd tools 
+rm revision
+rm /boot/image/*
+make install
+cp /boot/image/* /boot/image_big  # Make big image accessible by this name
+cp ../boot/boot/boot /boot/boot
+
+CC=clang make cleandepend clean depend image
 rm revision
 rm -rf /boot/minix/* # on old systems might not be present
 CC=clang make install
 cp ../sys/arch/i386/stand/boot/biosboot/boot_monitor /
 cp -rf /boot/minix/* /boot/minix_default
+
 cd /usr/src 
 if [ $MAKEMAP -ne 0 ]; then
 	find . -type f -perm 755 | xargs nm -n 2> /dev/null > symbols.txt
