a5190194a04b3ac9f041b7ca7fb4d780e408c062
Ben Gras <ben@minix3.org>
2012-03-06 17:57:34 +0100

    build: don't -L/usr/pkg/lib
    You might have to update the compiler-rt package! See UPDATING.
    
    	. the purpose of this -L was solely to find compiler-rt, which contains
    	  runtime support code for clang-compiled binaries
    	. this also makes all other packaged libraries visible, however
    	. it is cleaner to isolate the base system from packages, and so
    	  compiler-rt puts itself in /usr/pkg/compiler-rt/lib/ too, which the
    	  base system henceforth uses exclusively
    	. e.g. this solves a link failure when libfetch is installed as a
    	  package
    	. the new compiler-rt package also puts itself in /usr/pkg/lib for 'old'
    	  systems; that is harmless. The benefit of 'new' systems is that the other
    	  packages are hidden.

--- share/mk/minix.service.mk
+++ share/mk/minix.service.mk
@@ -4,7 +4,7 @@
 .if ${CC} == "gcc"
 LDADD+= -nodefaultlibs -lgcc -lsys -lgcc -lminc
 .elif ${CC} == "clang"
-LDADD+= -nodefaultlibs -L/usr/pkg/lib -L/usr/pkg/compiler-rt/lib -lCompilerRT-Generic -lsys -lCompilerRT-Generic -lminc
+LDADD+= -nodefaultlibs -L/usr/pkg/compiler-rt/lib -lCompilerRT-Generic -lsys -lCompilerRT-Generic -lminc
 .endif
 
 .include <bsd.prog.mk>
