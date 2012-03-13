6cdfedc12ce99200752a5fa1cc4432a88e1954a3
Ben Gras <ben@minix3.org>
2012-03-13 02:07:22 +0100

    tools: some nbsd_ports fixes
    	. figure out which dates were used for various nbsd imports
    	  and specify them for each one in the nbsd_ports file
    	. other minor fixes and improvements

--- tools/nbsd_diff.sh
+++ tools/nbsd_diff.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-echo "Updating $3"
-diff -ru $1 $2 | \
+diff=$3
+rm -f "$diff"
+if [ ! -d "$1" -o ! -d "$2" ]
+then	echo Skipping $diff
+	exit 0
+fi
+diff -aru $1 $2 | \
 	sed /"^Only in"/d | \
 	sed -e 's/^\(---.*\)\t.*/\1/' | \
-	sed -e 's/^\(\+\+\+.*\)\t.*/\1/' > $3
+	sed -e 's/^\(\+\+\+.*\)\t.*/\1/' > $diff
+
+if [ ! -s "$diff" ]
+then	rm -f "$diff"
+fi
