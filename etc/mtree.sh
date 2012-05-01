3baaafe11351dface3d1a25d02ecb6f2007a312a
Dirk Vogt <dirk@minix3.org>
2012-05-01 16:32:15 +0200

    When MKUNPRIV is set do not require root privilege

--- etc/mtree.sh
+++ etc/mtree.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 set -e
 
-if [ $# -ne 1 ]
-then	echo "Usage: $0 mtreefile"
+if [ $# -ne 5 ]
+then	echo "Usage: $0 mtreefile TOOL_AWK TOOL_STAT DEST_DIR UNPRIV"
 	exit 1
 fi
 
+AWK=$2
+STAT=$3
+DESTDIR=$4
+UNPRIV=$5
 cat "$1" | while read line
 do
-	NF="`echo $line | awk '{ print NF }'`"
+	NF="`echo $line | ${AWK} '{ print NF }'`"
 	if [ $NF = 4 ]
-	then	mode="`echo $line | awk '{ print $1 }'`"
-		owner="`echo $line | awk '{ print $2 }'`"
-		group="`echo $line | awk '{ print $3 }'`"
-		dir="`echo $line | awk '{ print $4 }'`"
+	then	mode="`echo $line | ${AWK} '{ print $1 }'`"
+		owner="`echo $line | ${AWK} '{ print $2 }'`"
+		group="`echo $line | ${AWK} '{ print $3 }'`"
+		dir="${DESTDIR}`echo $line | ${AWK} '{ print $4 }'`"
 		mkdir -p $dir
-		targetdev="`stat -f %d $dir/.`"
+		targetdev="`${STAT} -f %d $dir/.`"
 		if [ $targetdev -lt 256 ]
 		then	echo "skipping non-dev $dir properties"
-		else	chown $owner $dir
+		elif [ $UNPRIV != yes]
+		then	chown $owner $dir
 			chmod $mode $dir
 			chgrp $group $dir
 		fi
 	elif [ $NF = 3 ]
-	then	target="`echo $line | awk '{ print $3 }'`"
-		linkfile="`echo $line | awk '{ print $1 }'`"
+	then	target="`echo $line | ${AWK} '{ print $3 }'`"
+		linkfile="${DESTDIR}`echo $line | ${AWK} '{ print $1 }'`"
 		rm -f $linkfile
 		ln -s $target $linkfile
 	else	echo odd line.
