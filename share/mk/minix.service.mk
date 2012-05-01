7da8d39c066d10e2407f1828802945763279d31a
Dirk Vogt <dirk@minix3.org>
2012-05-01 16:57:11 +0200

    Fix compiler check

--- share/mk/minix.service.mk
+++ share/mk/minix.service.mk
@@ -1,9 +1,9 @@
 # MINIX-specific servers/drivers options
 .include <bsd.own.mk>
 
-.if ${CC} == "gcc"
+.if !empty(CC:M*gcc)
 LDADD+= -nodefaultlibs -lgcc -lsys -lgcc -lminc
-.elif ${CC} == "clang"
+.elif !empty(CC:M*clang)
 LDADD+= -nodefaultlibs -L/usr/pkg/compiler-rt/lib -lCompilerRT-Generic -lsys -lCompilerRT-Generic -lminc
 .endif
 
