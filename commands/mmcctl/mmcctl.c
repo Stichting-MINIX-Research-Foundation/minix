3411d6f99edf9060c130b01808079a4d57eda670
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-06-18 09:42:02 +0000

    MMC block driver development
    Change-Id: I409c0ba8e8f0e36fd6a6cc7f3f2b2644ea76785c

---
+++ commands/mmcctl/mmcctl.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#include <sys/ioc_mmc.h>
+
+
+static void show_usage()
+{
+	fprintf(stderr, "Usage: mmcctl device action\n");
+}
+
+int main(int argc, char **argv)
+{
+	int err;
+	int fd;
+	const char *device;
+	const char *action;
+	struct stat stats;
+
+	if (argc != 3) {
+		show_usage();
+		return EXIT_FAILURE;
+	}
+	device= argv[1];
+	action= argv[2];
+
+	err= stat(device, &stats);
+	if (err == -1) {
+		fprintf(stderr, "error can not open device '%s':%s\n", device,
+				strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	if (!S_ISBLK(stats.st_mode)) {
+		fprintf(stderr, "error device '%s' is not a block device\n", device);
+		return EXIT_FAILURE;
+	}
+
+	if (strncmp(action, "cid", 3) == 0) {
+		fprintf(stdout, "getting cid number\n");
+		fd= open(device, O_RDONLY);
+		if (fd == -1) {
+			fprintf(stderr, "error can not open device '%s':%s\n", device,
+					strerror(errno));
+			return EXIT_FAILURE;
+		}
+
+		uint8_t cid[16];
+		err= ioctl(fd, MIOGETCID, cid);
+		if (err) {
+			fprintf(stderr, "error doing ioctl on device '%s':%s\n", device,
+					strerror(errno));
+			close(fd);
+			return EXIT_FAILURE;
+		}
+		printf("CID:"
+				"%02X%02X%02X%02X"
+				"%02X%02X%02X%02X"
+				"%02X%02X%02X%02X"
+				"%02X%02X%02X%02X\n", cid[0], cid[1], cid[2], cid[3], cid[4],
+				cid[5], cid[6], cid[7], cid[8], cid[9], cid[10], cid[11],
+				cid[12], cid[13], cid[14], cid[15]);
+
+		close(fd);
+		return EXIT_SUCCESS;
+	}
+
+	fprintf(stdout, "mmcctl tool\n");
+	return EXIT_SUCCESS;
+}
---
+++ common/include/sys/ioc_mmc.h
@@ -0,0 +1,17 @@
+/*	sys/ioc_mmc.h - MMC ioctl() command codes.
+ *						  Author: Kees Jongenburger
+ *								31 Aug 2012
+ *
+ */
+
+#ifndef _S_I_MMC_H
+#define _S_I_MMC_H
+
+#include <minix/ioctl.h>
+
+typedef uint8_t cid[16]; 
+
+/* get the card CID */
+#define MIOGETCID _IOR('d',10,cid)
+
+#endif /* _S_I_MMC_H */
