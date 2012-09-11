660a22fb6aed005308fea2e2ad0274956cdea22b
Kees Jongenburger <kees.jongenburger>
2012-09-11 11:39:11 +0200

    indenting

--- commands/mmcctl/mmcctl.c
+++ commands/mmcctl/mmcctl.c
@@ -12,12 +12,14 @@
 
 #include <sys/ioc_mmc.h>
 
-static void show_usage()
+static void
+show_usage()
 {
 	fprintf(stderr, "Usage: mmcctl device action\n");
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
 	int err;
 	int fd;
@@ -35,12 +37,13 @@ int main(int argc, char **argv)
 	err = stat(device, &stats);
 	if (err == -1) {
 		fprintf(stderr, "error can not open device '%s':%s\n", device,
-				strerror(errno));
+		    strerror(errno));
 		return EXIT_FAILURE;
 	}
 
 	if (!S_ISBLK(stats.st_mode)) {
-		fprintf(stderr, "error device '%s' is not a block device\n", device);
+		fprintf(stderr, "error device '%s' is not a block device\n",
+		    device);
 		return EXIT_FAILURE;
 	}
 
@@ -48,26 +51,27 @@ int main(int argc, char **argv)
 		fprintf(stdout, "getting cid\n");
 		fd = open(device, O_RDONLY);
 		if (fd == -1) {
-			fprintf(stderr, "error can not open device '%s':%s\n", device,
-					strerror(errno));
+			fprintf(stderr, "error can not open device '%s':%s\n",
+			    device, strerror(errno));
 			return EXIT_FAILURE;
 		}
 
 		uint8_t cid[16];
 		err = ioctl(fd, MIOGETCID, cid);
 		if (err) {
-			fprintf(stderr, "error doing ioctl on device '%s':%s\n", device,
-					strerror(errno));
+			fprintf(stderr,
+			    "error doing ioctl on device '%s':%s\n", device,
+			    strerror(errno));
 			close(fd);
 			return EXIT_FAILURE;
 		}
 		printf("CID:"
-				"%02X%02X%02X%02X"
-				"%02X%02X%02X%02X"
-				"%02X%02X%02X%02X"
-				"%02X%02X%02X%02X\n", cid[0], cid[1], cid[2], cid[3], cid[4],
-				cid[5], cid[6], cid[7], cid[8], cid[9], cid[10], cid[11],
-				cid[12], cid[13], cid[14], cid[15]);
+		    "%02X%02X%02X%02X"
+		    "%02X%02X%02X%02X"
+		    "%02X%02X%02X%02X"
+		    "%02X%02X%02X%02X\n", cid[0], cid[1], cid[2], cid[3],
+		    cid[4], cid[5], cid[6], cid[7], cid[8], cid[9], cid[10],
+		    cid[11], cid[12], cid[13], cid[14], cid[15]);
 
 		close(fd);
 		return EXIT_SUCCESS;
