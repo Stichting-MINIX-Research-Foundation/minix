cae9a27e82b1c181cb0ccd36023e7fd7969f943b
Ben Gras <ben@minix3.org>
2012-09-14 18:19:54 +0200

    VM: static data structure for mem allocation
     . allocate physical memory using a fixed, pre-allocated bitmap so there
       are no call cycles and it's avilable earlier

--- servers/vm/addravl.c
+++
@@ -1,8 +0,0 @@
-
-#include "proto.h"
-#include "sanitycheck.h"
-#include "pagerange.h"
-#include "addravl_defs.h"
-#include "cavl_if.h"
-#include "cavl_impl.h"
-
--- servers/vm/addravl.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ADDRAVL
-#define ADDRAVL 1
-
-#include "addravl_defs.h"
-#include "cavl_if.h"
-#include "unavl.h"
-
-#endif
