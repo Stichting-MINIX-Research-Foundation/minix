aeda8e5c7b48d63030db8fa61505908913bfc184
Thomas Veerman <thomas@minix3.org>
2012-10-18 10:03:34 +0000

    Kernel: reenable interrupts before halting

--- kernel/arch/arm/arch_reset.c
+++ kernel/arch/arm/arch_reset.c
@@ -19,10 +19,12 @@
 #include "direct_utils.h"
 #include <machine/multiboot.h>
 
-void halt_cpu(void)
+void
+halt_cpu(void)
 {
-    asm volatile("dsb");
-    asm volatile("wfi");
+	asm volatile("dsb");
+	asm volatile("cpsie i");
+	asm volatile("wfi");
 }
 
 void
@@ -31,15 +33,17 @@ reset(void)
     while (1);
 }
 
-__dead void arch_shutdown(int how)
+__dead void
+arch_shutdown(int how)
 {
-    while (1);
+	while (1);
 }
 
 #ifdef DEBUG_SERIAL
-void ser_putc(char c)
+void
+ser_putc(char c)
 {
-    omap3_ser_putc(c);
+	omap3_ser_putc(c);
 }
 
 #endif
