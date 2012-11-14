10f30159a9ff4df1d759254499c65bdb5ad49e75
Tomas Hruby <tom@minix3.org>
2012-11-14 22:14:48 +0000

    struct minix_ipcvecs - pretty-print
    - the _ptr suffix is not needed anymore :-)

--- include/minix/ipc.h
+++ include/minix/ipc.h
@@ -173,13 +173,13 @@ int _minix_kernel_info_struct(struct minix_kerninfo **);
 #define senda           _senda
 
 struct minix_ipcvecs {
-	int (*send_ptr)(endpoint_t dest, message *m_ptr);
-	int (*receive_ptr)(endpoint_t src, message *m_ptr, int *st);
-	int (*sendrec_ptr)(endpoint_t src_dest, message *m_ptr);
-	int (*sendnb_ptr)(endpoint_t dest, message *m_ptr);
-	int (*notify_ptr)(endpoint_t dest);
-	int (*do_kernel_call_ptr)(message *m_ptr);
-	int (*senda_ptr)(asynmsg_t *table, size_t count);
+	int (*send)(endpoint_t dest, message *m_ptr);
+	int (*receive)(endpoint_t src, message *m_ptr, int *st);
+	int (*sendrec)(endpoint_t src_dest, message *m_ptr);
+	int (*sendnb)(endpoint_t dest, message *m_ptr);
+	int (*notify)(endpoint_t dest);
+	int (*do_kernel_call)(message *m_ptr);
+	int (*senda)(asynmsg_t *table, size_t count);
 };
 
 /* kernel-set IPC vectors retrieved by a constructor in libc/sys-minix/init.c */
@@ -187,37 +187,37 @@ extern struct minix_ipcvecs _minix_ipcvecs;
 
 static inline int _send(endpoint_t dest, message *m_ptr)
 {
-	return _minix_ipcvecs.send_ptr(dest, m_ptr);
+	return _minix_ipcvecs.send(dest, m_ptr);
 }
 
 static inline int _receive(endpoint_t src, message *m_ptr, int *st)
 {
-	return _minix_ipcvecs.receive_ptr(src, m_ptr, st);
+	return _minix_ipcvecs.receive(src, m_ptr, st);
 }
 
 static inline int _sendrec(endpoint_t src_dest, message *m_ptr)
 {
-	return _minix_ipcvecs.sendrec_ptr(src_dest, m_ptr);
+	return _minix_ipcvecs.sendrec(src_dest, m_ptr);
 }
 
 static inline int _sendnb(endpoint_t dest, message *m_ptr)
 {
-	return _minix_ipcvecs.send_ptr(dest, m_ptr);
+	return _minix_ipcvecs.send(dest, m_ptr);
 }
 
 static inline int _notify(endpoint_t dest)
 {
-	return _minix_ipcvecs.notify_ptr(dest);
+	return _minix_ipcvecs.notify(dest);
 }
 
 static inline int do_kernel_call(message *m_ptr)
 {
-	return _minix_ipcvecs.do_kernel_call_ptr(m_ptr);
+	return _minix_ipcvecs.do_kernel_call(m_ptr);
 }
 
 static inline int _senda(asynmsg_t *table, size_t count)
 {
-	return _minix_ipcvecs.senda_ptr(table, count);
+	return _minix_ipcvecs.senda(table, count);
 }
 
 #endif /* _IPC_H */
