42b4cf97fd28f995493de1df0a9ea6cc9b0f4d43
Ben Gras <ben@minix3.org>
2013-03-20 17:04:39 +0000

    filesystems: return ENOSYS for REQ_PEEK
    This indicates to VFS that they are not annotating their cache
    blocks (in VM) with inode number/offset info, and therefore mmap()
    shouldn't succeed on any of their files. (Most importantly exec()
    won't fallback elegantly otherwise.)
    
    Change-Id: Ic57ee422864b4bbc031eadba32973270907b02fd

--- servers/mfs/table.c
+++ servers/mfs/table.c
@@ -44,6 +44,6 @@ int (*fs_call_vec[])(void) = {
         fs_rdlink,	    /* 30  */
         fs_getdents,	    /* 31  */
         fs_statvfs,         /* 32  */
-        fs_readwrite,       /* 33  */
+        no_sys,             /* 33  */		/* peek not supported */
 };
 
