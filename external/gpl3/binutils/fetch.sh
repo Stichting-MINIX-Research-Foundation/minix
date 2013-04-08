c2e1e103053cae4a2c5abfbec2c95d5b2e74c463
Lionel Sambuc <lionel.sambuc@gmail.com>
2013-04-08 23:11:36 +0200

    Upgrading to binutils-2.22
     * Added --with-ld=default
     * Added --with-lto
     * Options are prepared for future activation of gold.
    
    Change-Id: Id9618904055e18d1b37f5e9585f775c367ff356e

--- external/gpl3/binutils/fetch.sh
+++ external/gpl3/binutils/fetch.sh
@@ -5,8 +5,8 @@ echo $0
 cd `dirname $0`
 
 # Configure fetch method
-URL="http://www.minix3.org/distfiles-minix/binutils-2.21.1.tar.bz2"
-BACKUP_URL="http://ftp.gnu.org/gnu/binutils/binutils-2.21.1.tar.bz2"
+URL="http://www.minix3.org/distfiles-minix/binutils-2.22.tar.bz2"
+BACKUP_URL="http://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2"
 FETCH=wget
 which curl >/dev/null
 if [ $? -eq 0 ]; then
@@ -16,15 +16,15 @@ fi
 # Fetch sources if not available
 if [ ! -d dist ];
 then
-	if [ ! -f binutils-2.21.1.tar.bz2 ]; then
+	if [ ! -f binutils-2.22.tar.bz2 ]; then
 		$FETCH $URL
 		if [ $? -ne 0 ]; then
 			$FETCH $BACKUP_URL
 		fi
 	fi
 
-	tar -oxjf binutils-2.21.1.tar.bz2 && \
-	mv binutils-2.21.1 dist && \
+	tar -oxjf binutils-2.22.tar.bz2 && \
+	mv binutils-2.22 dist && \
 	cd dist && \
 	cat ../patches/* | patch -p1
 fi
