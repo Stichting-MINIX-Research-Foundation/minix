e946cc362de24792ead9d3f99de99f0fc8ee7fcf
Lionel Sambuc <lionel@minix3.org>
2013-04-10 18:13:48 +0200

    Importing texinfo
    Also adapted the fetch rules to correctly generate the gitignore
    files for gcc, and allow the case of multiple modules in the same
    directory, as found in gnu/dist.
    
    Warning: This patch has an entry in docs/UPDATING
    
    Change-Id: Ib781734e8fd7f9c6265fa65d62ba2cf3fccbc5ba

--- gnu/dist/fetch.sh
+++ gnu/dist/fetch.sh
@@ -4,7 +4,7 @@
 echo $0
 cd `dirname $0`
 
-# Configure fetch method
+# Configure fetch method - GMAKE
 URL="http://www.minix3.org/distfiles-minix/make-3.80.tar.bz2"
 BACKUP_URL="ftp://ftp.gnu.org/gnu/make/make-3.80.tar.bz2"
 FETCH=wget
@@ -24,6 +24,34 @@ then
 	fi
 
 	tar -xjf make-3.80.tar.bz2 && \
-	mv make-3.80 gmake
+	mv make-3.80 gmake && \
+	echo "make*" >> .gitignore
+	echo "gmake*" >> .gitignore
+fi
+
+# Configure fetch method - TEXINFO
+URL="http://www.minix3.org/distfiles-minix/texinfo-4.8.tar.bz2"
+BACKUP_URL="ftp://ftp.gnu.org/gnu/texinfo/texinfo-4.8.tar.bz2"
+FETCH=wget
+which curl >/dev/null
+if [ $? -eq 0 ]; then
+	FETCH="curl -O -f"
+fi
+
+# Fetch sources if not available
+if [ ! -d texinfo ];
+then
+	if [ ! -f texinfo-4.8.tar.bz2 ]; then
+		$FETCH $URL
+		if [ $? -ne 0 ]; then
+			$FETCH $BACKUP_URL
+		fi
+	fi
+
+	tar -xjf texinfo-4.8.tar.bz2 && \
+	cd texinfo-4.8 && \
+	cat ../../usr.bin/texinfo/patches/* | patch -p1 && \
+	cd - && \
+	mv texinfo-4.8 texinfo
 fi
 
