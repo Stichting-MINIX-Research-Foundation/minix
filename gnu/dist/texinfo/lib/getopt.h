08aed28df61ba2fc4e589752aa320891c6180b22
Lionel Sambuc <lionel@minix3.org>
2013-04-10 18:13:48 +0200

    Importing texinfo
    Change-Id: Ib781734e8fd7f9c6265fa65d62ba2cf3fccbc5ba

---
+++ gnu/dist/texinfo/lib/getopt.h
@@ -0,0 +1,12 @@
+/*	$NetBSD: getopt.h,v 1.1.1.6 2008/09/02 07:49:28 christos Exp $	*/
+
+/* getopt.h -- wrapper for gnulib getopt_.h.
+   Id: getopt.h,v 1.6 2004/09/14 12:36:00 karl Exp
+   Copyright (C) 2004 Free Software Foundation, Inc.
+
+   Copying and distribution of this file, with or without modification,
+   are permitted in any medium without royalty provided the copyright
+   notice and this notice are preserved.
+*/
+
+#include "getopt_.h"
