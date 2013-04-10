b3731794e2752ead2af22a4bd6225477de540976
Lionel Sambuc <lionel@minix3.org>
2013-04-10 18:13:48 +0200

    Importing texinfo
    Change-Id: Ib781734e8fd7f9c6265fa65d62ba2cf3fccbc5ba

---
+++ gnu/dist/texinfo/lib/memcpy.c
@@ -0,0 +1,40 @@
+/*	$NetBSD: memcpy.c,v 1.1.1.3 2004/07/12 23:26:57 wiz Exp $	*/
+
+/* Copyright (C) 1995, 1997, 2000, 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* Written by Jim Meyering <meyering@na-net.ornl.gov>.  */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stddef.h>
+
+/* Copy LEN bytes starting at SRCADDR to DESTADDR.  Result undefined
+   if the source overlaps with the destination.
+   Return DESTADDR. */
+
+void *
+memcpy (void *destaddr, void const *srcaddr, size_t len)
+{
+  char *dest = destaddr;
+  char const *src = srcaddr;
+
+  while (len-- > 0)
+    *dest++ = *src++;
+  return destaddr;
+}
---
+++ gnu/dist/texinfo/lib/strncasecmp.c
@@ -0,0 +1,4 @@
+/*	$NetBSD: strncasecmp.c,v 1.1.1.2 2003/01/17 14:54:30 wiz Exp $	*/
+
+#define LENGTH_LIMIT
+#include "strcasecmp.c"
