8d4253e6fdd0af97ed3f0a282c3b510f534ffa3d
Ben Gras <ben@minix3.org>
2013-04-15 17:44:19 +0000

    vfs: fix null deref, pfs: add fchmod()
    	. vfs read_only() assumes vnode->v_vmnt is non-NULL, but it can
    	  be NULL sometimes
    	. e.g. fchmod() on UDS triggered NULL deref; add a check and
    	  add REQ_CHMOD to pfs so unix domain sockets can be fchmod()ded
    	. add to test56
    
    Change-Id: I83c840f101b647516897cc99fcf472116d762012

--- servers/pfs/misc.c
+++ servers/pfs/misc.c
@@ -1,4 +1,5 @@
 #include "fs.h"
+#include "inode.h"
 
 
 /*===========================================================================*
@@ -9,4 +10,19 @@ int fs_sync(message *fs_m_in, message *fs_m_out)
 /* Perform the sync() system call.  No-op on this FS. */
 
   return(OK);		/* sync() can't fail */
+}
+
+/*===========================================================================*
+ *                             fs_chmod					     *
+ *===========================================================================*/
+int fs_chmod(message *fs_m_in, message *fs_m_out)
+{
+  struct inode *rip;  /* target inode */
+  mode_t mode = (mode_t) fs_m_in->REQ_MODE;
+
+  if( (rip = find_inode(fs_m_in->REQ_INODE_NR)) == NULL) return(EINVAL);
+  get_inode(rip->i_dev, rip->i_num);	/* mark inode in use */
+  rip->i_mode = (rip->i_mode & ~ALL_MODES) | (mode & ALL_MODES);
+  put_inode(rip);			/* release the inode */
+  return OK;
 }
