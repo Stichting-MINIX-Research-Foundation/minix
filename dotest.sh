3f5339df974598b554b1e70ee2dee9d9ef85134a
Kees Jongenburger <keesj@minix3.org>
2013-04-16 13:50:32 +0200

    Adding dotest
    test
    Change-Id: Ic71e59f0de7f3be1b1443416179151e20b2b0aa7

---
+++ dotest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export PATH=../obj.evbearm-el/tooldir.Linux-3.5.0-26-generic-x86_64/bin/:$PATH
+
+asf
+sdaf
+#
+# Convert the METALOG to something else
+# -N etc points to the master.passwd and groups file
+cat ../obj.evbearm-el/destdir.evbearm-el/METALOG.sanitised | nbmtree -N etc -C > input
+#nbmake-evbearm-el -C tools/mkfs.mfs all install
+#nbmkfs.mfs image ../obj.evbearm-el/destdir.evbearm-el/METALOG
