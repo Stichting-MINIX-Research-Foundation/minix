2ec49130c1745973761153005f9f6ad0e938dae8
Lionel Sambuc <lionel@minix3.org>
2013-04-17 13:01:55 +0200

    fix sysroot clang
    Change-Id: I63adf8460e7fdbe3d9d5ad6c828ae5758cb1643c

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -97,14 +97,22 @@ typedef	uint64_t	u_int64_t;
 
 #ifdef __minix
 typedef uint8_t 	u8_t;
+#define u8_t		uint8_t
 typedef uint16_t	u16_t;
+#define u16_t		uint16_t
 typedef uint32_t	u32_t;
+#define u32_t		uint32_t
 typedef uint64_t 	u64_t;
+#define u64_t		uint64_t
 
 typedef int8_t		i8_t;
+#define i8_t		int8_t
 typedef int16_t		i16_t;
+#define i16_t		int16_t
 typedef int32_t		i32_t;
+#define i32_t		int32_t
 typedef int64_t		i64_t;
+#define i64_t		int64_t
 
 #ifdef _NETBSD_SOURCE
 /* some Minix specific types that do not conflict with posix */
