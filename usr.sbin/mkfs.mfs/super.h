49fe5d161717795c22247c4e042a78f850d91040
Antoine Leca <antoine.leca.1@gmail.com>
2013-04-26 20:54:22 +0200

    mkfs reloaded
    * restored the zone>block stuff
    * generalized the support for more-than-1-block bitmaps to also deal
      with big inodes maps in small blocks
    * improved pexit() to printf-like syntax; also replaced pexit() with
      err() or errx() where there are no reference to proto lines
    * unified the allocation of blocks to all use alloc_block() instead
      of raw malloc half of the time
    * removed the V2_ d2_ etc. prefixes which are obscure and obsolete
    
    While here, also
    * used new-form definitions, and closer to KNF style in general
    * used often-built-ins such as mem* or strncpy where relevant
    * pruned a fair amount of constants which are irrelevant
    
    Thanks to Thomas V. and Lionel who did a good amount of work with the
    cross-compilation stuff and made that change to be much smoother.

--- usr.sbin/mkfs.mfs/super.h
+++ usr.sbin/mkfs.mfs/super.h
@@ -1,8 +1,7 @@
-#ifndef __MFS_SUPER_H__
-#define __MFS_SUPER_H__
+#ifndef _MKFS_MFS_SUPER_H__
+#define _MKFS_MFS_SUPER_H__
 
-/* Super block table.  The root file system and every mounted file system
- * has an entry here.  The entry holds information about the sizes of the bit
+/* Super block table.  The entry holds information about the sizes of the bit
  * maps and inodes.  The s_ninodes field gives the number of inodes available
  * for files and directories, including the root directory.  Inode 0 is 
  * on the disk, but not used.  Thus s_ninodes = 4 means that 5 bits will be
@@ -17,11 +16,9 @@
  *    inodes        (s_ninodes + 'inodes per block' - 1)/'inodes per block'
  *    unused        whatever is needed to fill out the current zone
  *    data zones    (s_zones - s_firstdatazone) << s_log_zone_size
- *
- * A super_block slot is free if s_dev == NO_DEV. 
  */
 
-EXTERN struct super_block {
+struct super_block {
   uint32_t s_ninodes;		/* # usable inodes on the minor device */
   uint16_t  s_nzones;		/* total device size, including bit maps etc */
   int16_t s_imap_blocks;	/* # of blocks used by inode bit map */
@@ -35,37 +32,21 @@ EXTERN struct super_block {
 
   /* The following items are valid on disk only for V3 and above */
 
-  /* The block size in bytes. Minimum MIN_BLOCK SIZE. SECTOR_SIZE
-   * multiple. If V1 or V2 filesystem, this should be
-   * initialised to STATIC_BLOCK_SIZE.
-   */
   int16_t s_pad2;		/* try to avoid compiler-dependent padding */
+  /* The block size in bytes. Minimum MIN_BLOCK SIZE. SECTOR_SIZE multiple.*/
   uint16_t s_block_size;	/* block size in bytes. */
   int8_t s_disk_version;	/* filesystem format sub-version */
 
   /* The following items are only used when the super_block is in memory.
    * If this ever changes, i.e. more fields after s_disk_version has to go to
-   * disk, update LAST_ONDISK_FIELD in super.c as that controls which part of the
-   * struct is copied to and from disk.
+   * disk, update LAST_ONDISK_FIELD in servers/mfs/super.c as that controls
+   * which part of the struct is copied to and from disk.
    */
-  
-  /*struct inode *s_isup;*/	/* inode for root dir of mounted file sys */
-  /*struct inode *s_imount;*/   /* inode mounted on */
+/* XXX padding inserted here... */
+
   unsigned s_inodes_per_block;	/* precalculated from magic number */
   uint32_t s_firstdatazone;	/* number of first data zone (big) */
-  dev_t s_dev;			/* whose super block is this? */
-  int32_t s_rd_only;		/* set to 1 iff file sys mounted read only */
-  int32_t s_native;		/* set to 1 iff not byte swapped file system */
-  int32_t s_version;		/* file system version, zero means bad magic */
-  int32_t s_ndzones;		/* # direct zones in an inode */
-  int32_t s_nindirs;		/* # indirect zones per indirect block */
-  uint32_t s_isearch;		/* inodes below this bit number are in use */
-  uint32_t s_zsearch;		/* all zones below this bit number are in use*/
-  int8_t s_is_root;
 } superblock;
-
-#define IMAP		0	/* operating on the inode bit map */
-#define ZMAP		1	/* operating on the zone bit map */
 
 /* s_flags contents; undefined flags are guaranteed to be zero on disk
  * (not counting future versions of mfs setting them!)
@@ -79,5 +60,15 @@ EXTERN struct super_block {
  */
 #define MFSFLAG_MANDATORY_MASK 0xff00
 
-#endif
+/* The block size should be registered on disk, even
+ * multiple. If V1 or V2 filesystem, this should be
+ * initialised to STATIC_BLOCK_SIZE.
+ */
+#define	MFS_SUPER_BLOCK_SIZE	s_block_size
 
+/* To keep the super block on disk clean, the MFS server only read/write up to
+ * and including this field:
+ */
+#define LAST_ONDISK_FIELD	s_disk_version
+
+#endif
