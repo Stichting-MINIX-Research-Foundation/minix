ba04ce764d5b2ddf92056a25c4de077b32a384d8
Kees Jongenburger <kees.jongenburger@gmail.com>
2013-05-23 10:45:38 +0200

    arm:keep kernel and modules in the first 256MB of memory.
    Keep kernel and modules in the first 256MB of memory in preparation
    for the beaglebone. That target only has 256 MB of memory.
    
    Change-Id: I3d92247b5d4e5d3aab7388fe01c2f5713d6a4593

--- releasetools/gen_uEnv.txt.sh
+++ releasetools/gen_uEnv.txt.sh
@@ -2,19 +2,19 @@
 
 #generate a u-boot u-env.
 list="0x80200000 kernel.bin
-0x90000000 ds.elf
-0x90800000 rs.elf
-0x91000000 pm.elf
-0x91800000 sched.elf
-0x92000000 vfs.elf
-0x92800000 memory.elf
-0x93000000 log.elf
-0x93800000 tty.elf
-0x94000000 mfs.elf
-0x94800000 vm.elf
-0x95000000 pfs.elf
-0x95800000 init.elf
-0x96000000 cmdline.txt"
+0x82000000 ds.elf
+0x82800000 rs.elf
+0x83000000 pm.elf
+0x83800000 sched.elf
+0x84000000 vfs.elf
+0x84800000 memory.elf
+0x85000000 log.elf
+0x85800000 tty.elf
+0x86000000 mfs.elf
+0x86800000 vm.elf
+0x87000000 pfs.elf
+0x87800000 init.elf
+0x88000000 cmdline.txt"
 
 #
 # PREFIX for loading file over tftp to allow hosting multiple
@@ -63,12 +63,12 @@ echo "# Set the command to be executed"
 echo "uenvcmd=run $BOOT"
 echo 
 echo "# With cmdline/bootargs in cmdline.txt"
-echo "mmcbootcmd=echo starting from MMC ; mmc part 0; mw.b 0x96000000 0 16384 $(fill_cmd "fatload mmc 0:1" "") ; dcache off ; icache off ; go 0x80200000"
+echo "mmcbootcmd=echo starting from MMC ; mmc part 0; mw.b 0x88000000 0 16384 $(fill_cmd "fatload mmc 0:1" "") ; dcache off ; icache off ; go 0x80200000"
 echo 
 echo "# Netbooting."
 echo "serverip=192.168.12.10"
 echo "ipaddr=192.168.12.62"
 echo "usbnet_devaddr=e8:03:9a:24:f9:10"
 echo "usbethaddr=e8:03:9a:24:f9:11"
-echo "netbootcmd=echo starting from TFTP; usb start; mw.b 0x96000000 0 16384 $(fill_cmd "tftp" "$NETBOOT_PREFIX") ; dcache off ; icache off ; go 0x80200000"
+echo "netbootcmd=echo starting from TFTP; usb start; mw.b 0x88000000 0 16384 $(fill_cmd "tftp" "$NETBOOT_PREFIX") ; dcache off ; icache off ; go 0x80200000"
 exit 0
