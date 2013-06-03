7f62331f394e68bbe1726aa2a0bbd4b890a67df1
Ben Gras <ben@minix3.org>
2013-06-03 10:54:08 +0000

    netbsd ping & traceroute
    	. remove minix ping
    	. add support for socket(AF_INET, SOCK_RAW, {IPPROTO_ICMP,IPPROTO_UDP})
    	. gives test48 a better chance of detecting network connectivity
    
    Change-Id: Ia678546d27ac378642f1160a141e1fc33458cce2

--- minix/include/net/gen/inet.h
+++ minix/include/net/gen/inet.h
@@ -9,7 +9,5 @@ server/ip/gen/inet.h
 
 ipaddr_t inet_addr( const char *addr );
 ipaddr_t inet_network( const char *addr );
-char *inet_ntoa( ipaddr_t addr );
-int inet_aton( const char *cp, ipaddr_t *pin );
 
 #endif /* __SERVER__IP__GEN__INET_H__ */
