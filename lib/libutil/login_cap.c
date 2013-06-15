d1b3ab953e42970d043bc938bf8bbf2b5454ecdd
Ben Gras <ben@minix3.org>
2013-06-15 23:43:39 +0000

    a few DBG=-g build fixes, both arches
    	- setlogin() not available
    	- softfloat needed in -lminc for arm
    	- libminc: we have to set LIBCDIR for the included files,
    	  so use that instead of LIBCSRCDIR
    
    Change-Id: I7f92621ebbca9ce08dc377b3fa61dee089071757

--- lib/libutil/login_cap.c
+++ lib/libutil/login_cap.c
@@ -700,6 +700,7 @@ setusercontext(login_cap_t *lc, struct passwd *pwd, uid_t uid, u_int flags)
 	}
 	errno = 0;
 
+#ifndef __minix
 	if (flags & LOGIN_SETLOGIN)
 		if (setlogin(pwd->pw_name) == -1) {
 			syslog(LOG_ERR, "setlogin(%s) failure: %m",
@@ -707,6 +708,7 @@ setusercontext(login_cap_t *lc, struct passwd *pwd, uid_t uid, u_int flags)
 			login_close(flc);
 			return (-1);
 		}
+#endif
 
 	if (flags & LOGIN_SETUSER)
 		if (setuid(uid) == -1) {
