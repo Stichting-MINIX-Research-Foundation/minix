c37e694364b35fd535ac42dcc8c17f1780b87d40
Ben Gras <ben@minix3.org>
2013-08-26 16:43:16 +0000

    enable fatal warnings
    (debug build for jenkins builds with MAKEVERBOSE=2. hopefully.)
    
    Change-Id: I71e228c87a7226adbbd940ccb1439e042d7b6e9a

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -85,12 +85,6 @@ LDS_RELOC=		${LDS_PATH}/elf_${MACHINE_ARCH}_minix.xr
 LDS_SHARED_LIB=		${LDS_PATH}/elf_${MACHINE_ARCH}_minix.xsc
 LDS_N=			${LDS_PATH}/elf_${MACHINE_ARCH}_minix.xbn
 
-# LSC In the current state there is too much to be done
-# Some package have been identified by directly adding NOGCCERROR
-# To their Makefiles
-NOGCCERROR?=	yes
-NOCLANGERROR?=	yes
-
 AFLAGS+=	-D__ASSEMBLY__
 CFLAGS+=	-fno-builtin
 
@@ -1371,7 +1365,7 @@ MAKEDIRTARGET=\
 #	3	Ignore the effect of the "@" prefix in make commands
 #	4	Trace shell commands using the shell's -x flag
 #		
-MAKEVERBOSE?=		2
+MAKEVERBOSE:=		2
 
 .if ${MAKEVERBOSE} == 0
 _MKMSG?=	@\#
