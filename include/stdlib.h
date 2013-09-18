d266a94fea1894b1876270bf0a3daae8ccc22447
David van Moolenbroek <david@minix3.org>
2013-09-18 13:38:36 +0200

    Enable devname(3)
    There is no support for a device name database yet, so this call is
    expected to be fairly slow.
    
    Change-Id: I73aa5f267e2b6921b7d3bbdcc4beac463931132c

--- include/stdlib.h
+++ include/stdlib.h
@@ -272,12 +272,10 @@ int	 cgetustr(char *, const char *, char **);
 void	 csetexpandtc(int);
 
 int	 daemon(int, int);
-#ifndef __minix
 int	 devname_r(dev_t, mode_t, char *, size_t);
 #ifndef __LIBC12_SOURCE__
 __aconst char *devname(dev_t, mode_t) __RENAME(__devname50);
 #endif
-#endif /* !__minix */
 
 #define	HN_DECIMAL		0x01
 #define	HN_NOSPACE		0x02
