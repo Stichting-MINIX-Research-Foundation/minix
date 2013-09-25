a17691a98f585e72b250cfb2641fcb7e229d95fe
Kees Jongenburger <kees.jongenburger@gmail.com>
2013-09-25 09:47:27 +0200

    arm:define the physical memory range for the omap.
    Change-Id: I256dbeb58fec5b95b48d521054810fe478fce67f

--- include/arch/earm/include/memory.h
+++ include/arch/earm/include/memory.h
@@ -3,4 +3,10 @@
 #ifndef _ARM_MEMORY_H
 #define _ARM_MEMORY_H
 
+#if defined(DM37XX) || defined(AM335X)
+/* omap */
+#define PHYS_MEM_BEGIN 0x80000000
+#define PHYS_MEM_END 0xbfffffff
+#endif  /* defined(DM37XX) || defined(AM335X) */
+
 #endif /* _ARM_MEMORY_H */
