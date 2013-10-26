30312dd70fc2213803d28152ab4e3854e3b4ffbd
Jan Wieck <janwieck@yahoo.com>
2013-10-26 11:53:00 +0000

    Add UDP support to getsockname(2) and getpeername(2).

--- lib/libc/sys-minix/getsockname.c
+++ lib/libc/sys-minix/getsockname.c
@@ -19,6 +19,7 @@
 #include <net/gen/tcp.h>
 #include <net/gen/tcp_io.h>
 #include <net/gen/udp.h>
+#include <net/gen/udp_io.h>
 #include <sys/un.h>
 
 /*
@@ -28,6 +29,9 @@
 static int _tcp_getsockname(int fd, struct sockaddr *__restrict address,
    socklen_t *__restrict address_len, nwio_tcpconf_t *tcpconfp);
 
+static int _udp_getsockname(int fd, struct sockaddr *__restrict address,
+   socklen_t *__restrict address_len, nwio_udpopt_t *udpopt);
+
 static int _uds_getsockname(int fd, struct sockaddr *__restrict address,
    socklen_t *__restrict address_len, struct sockaddr_un *uds_addr);
 
@@ -36,6 +40,7 @@ int getsockname(int fd, struct sockaddr *__restrict address,
 {
 	int r;
 	nwio_tcpconf_t tcpconf;
+	nwio_udpopt_t udpopt;
 	struct sockaddr_un uds_addr;
 
 #ifdef DEBUG
@@ -52,6 +57,18 @@ int getsockname(int fd, struct sockaddr *__restrict address,
 		}
 
 		return _tcp_getsockname(fd, address, address_len, &tcpconf);
+	}
+
+	r= ioctl(fd, NWIOGUDPOPT, &udpopt);
+	if (r != -1 || (errno != ENOTTY && errno != EBADIOCTL))
+	{
+		if (r == -1)
+		{
+			/* Bad file descriptor */
+			return -1;
+		}
+
+		return _udp_getsockname(fd, address, address_len, &udpopt);
 	}
 
 	r= ioctl(fd, NWIOGUDSADDR, &uds_addr);
@@ -83,17 +100,46 @@ static int _tcp_getsockname(int fd, struct sockaddr *__restrict address,
 
 #ifdef DEBUG1
 	fprintf(stderr, "mnx_getsockname: from %s, %u",
-			inet_ntoa(tcpconf.nwtc_remaddr),
-			ntohs(tcpconf.nwtc_remport));
+			inet_ntoa(tcpconf->nwtc_remaddr),
+			ntohs(tcpconf->nwtc_remport));
 	fprintf(stderr," for %s, %u\n",
-			inet_ntoa(tcpconf.nwtc_locaddr),
-			ntohs(tcpconf.nwtc_locport));
+			inet_ntoa(tcpconf->nwtc_locaddr),
+			ntohs(tcpconf->nwtc_locport));
 #endif
 
 	memset(&sin, '\0', sizeof(sin));
 	sin.sin_family= AF_INET;
 	sin.sin_addr.s_addr= tcpconf->nwtc_locaddr ;
 	sin.sin_port= tcpconf->nwtc_locport;
+
+	len= *address_len;
+	if (len > sizeof(sin))
+		len= sizeof(sin);
+	memcpy(address, &sin, len);
+	*address_len= len;
+
+	return 0;
+}
+
+static int _udp_getsockname(int fd, struct sockaddr *__restrict address,
+   socklen_t *__restrict address_len, nwio_udpopt_t *udpopt)
+{
+	socklen_t len;
+	struct sockaddr_in sin;
+
+#ifdef DEBUG1
+	fprintf(stderr, "mnx_getsockname: from %s, %u",
+			inet_ntoa(udpopt->nwuo_remaddr),
+			ntohs(udpopt->nwuo_remport));
+	fprintf(stderr," for %s, %u\n",
+			inet_ntoa(udpopt->nwuo_locaddr),
+			ntohs(udpopt->nwuo_locport));
+#endif
+
+	memset(&sin, '\0', sizeof(sin));
+	sin.sin_family= AF_INET;
+	sin.sin_addr.s_addr= udpopt->nwuo_locaddr ;
+	sin.sin_port= udpopt->nwuo_locport;
 
 	len= *address_len;
 	if (len > sizeof(sin))
