848ad229ecaab7317490b813924099c3b0dd33b2
Lionel Sambuc <lionel@minix3.org>
2013-11-19 15:26:47 +0100

    libc/sys-minix/mount.c: fix overflow
    Fix a bug where a filesystem label could overflow the reserved buffer.
    This was already possible with 32 bits values, but is more proeminent
    with dev_t being 64 bits.
    
    Change-Id: Idc04ed355d1dd92b7a8ce4699de832661a5c4ccd

--- include/minix/mount.h
+++ include/minix/mount.h
@@ -9,6 +9,8 @@
 #define MS_REUSE	0x001	/* Tell RS to try reusing binary from memory */
 #define MS_EXISTING	0x002	/* Tell mount to use already running server */
 
+#define MNT_LABEL_LEN	16	/* Length of fs label including nul */
+
 /* Legacy definitions. */
 #define MNTNAMELEN	16	/* Length of fs type name including nul */
 #define MNTFLAGLEN	64	/* Length of flags string including nul */
