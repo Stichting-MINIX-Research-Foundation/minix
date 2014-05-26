a1d08ebddc22f3ef8bc635c26fea51f82835d42e
Kees Jongenburger <kees.jongenburger@gmail.com>
2014-05-26 16:47:43 +0200

    Adding headers for libdde (not strictly part of ddekit)
    Change-Id: I87ad9df2034e70eea857d436862ab89d5c0c707f
    
    http://gerrit.minix3.org/#/c/2559/

---
+++ include/libdde/usb_server.h
@@ -0,0 +1,7 @@
+#ifndef __LIBDDE_USB_SERVER_H__
+#define __LIBDDE_USB_SERVER_H__
+/*****************************************************************************
+ *         ddekit_usb_server_init                                            *
+ ****************************************************************************/
+void ddekit_usb_server_init();
+#endif /* __LIBDDE_USB_SERVER_H__ */
