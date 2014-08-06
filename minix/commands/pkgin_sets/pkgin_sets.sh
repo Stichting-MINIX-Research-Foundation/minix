aea6d8c9224994204340d02732bae38e5f5546b2
Lionel Sambuc <lionel@minix3.org>
2014-08-06 17:29:21 +0200

    Introduce pkgin_sets
    This tool allow you to install one or more predefined package sets.
    
    Change-Id: Id21c9e9fbd43f44f2d3b040961411428e1ae0af9

---
+++ minix/commands/pkgin_sets/pkgin_sets.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+CORE_TOOLS="openssh vim curl"
+DEV_TOOLS="git-base bmake gmake binutils clang"
+EXTRA_TOOLS="bison groff perl python"
+
+cat <<START_EOT
+This script contains 3 sets of packages, you can install any of those
+sets independently, just type N when pkgin ask for confirmation to skip
+a set.
+
+The following sets are available:
+
+1. Core tools:
+${CORE_TOOLS}
+
+2. Dev tools:
+${DEV_TOOLS}
+
+3. Extras:
+${EXTRA_TOOLS}
+
+START_EOT
+
+echo "Installing core tools"
+echo "====================="
+echo
+
+pkgin install ${CORE_TOOLS}
+
+echo
+echo "Installing default development tools"
+echo "===================================="
+echo
+
+pkgin install ${DEV_TOOLS}
+
+echo
+echo "Installing extras"
+echo "================="
+echo
+
+pkgin install ${EXTRA_TOOLS}
