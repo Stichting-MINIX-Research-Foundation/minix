5f5dcc545f54b4b1cc77676a01888a80436d24a9
Lionel Sambuc <lionel@minix3.org>
2014-08-21 13:35:45 +0200

    Fix bitcode support for kernel

--- minix/kernel/usermapped_data.c
+++ minix/kernel/usermapped_data.c
@@ -1,11 +1,11 @@
 #include "kernel/kernel.h"
 
 /* This is the user-visible struct that has pointers to other bits of data. */
-struct minix_kerninfo minix_kerninfo;
+struct minix_kerninfo minix_kerninfo __section(".usermapped");
 
 /* Kernel information structures. */
-struct kinfo kinfo;               /* kernel information for users */
-struct machine machine;           /* machine information for users */
-struct kmessages kmessages;       /* diagnostic messages in kernel */
-struct loadinfo loadinfo;        /* status of load average */
+struct kinfo kinfo __section(".usermapped");		/* kernel information for users */
+struct machine machine __section(".usermapped");	/* machine information for users */
+struct kmessages kmessages __section(".usermapped");	/* diagnostic messages in kernel */
+struct loadinfo loadinfo __section(".usermapped");	/* status of load average */
 
