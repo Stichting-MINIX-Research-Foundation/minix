2909efcb8835af7f12e5e7b21c5fc98177239fb2
Ben Gras <ben@minix3.org>
2014-08-22 12:34:33 +0200

    fix test48 - minix3.org.xyz resolves now
    Change-Id: I40b86b7c8cbfcbac0c976c2477200081c2cfc8f7

--- minix/tests/test48.c
+++ minix/tests/test48.c
@@ -318,7 +318,7 @@ static struct
 	{ "static.minix3.org",     0xC023C00A, 0, 1, 1, 0,                },
 	{ "",               0x00000000, 1, 0, 0, (1<<EAI_NONAME)|(1<<EAI_FAIL)|(1<<EAI_NODATA)},
 	{ "256.256.256.256",0x00000000, 1, 0, 0, (1<<EAI_NONAME)|(1<<EAI_FAIL)|(1<<EAI_NODATA)},
-	{ "minix3.xyz",     0x00000000, 0, 0, 1, (1<<EAI_NONAME)|(1<<EAI_FAIL)|(1<<EAI_NODATA)}};
+	{ "minix3.example.com",     0x00000000, 0, 0, 1, (1<<EAI_NONAME)|(1<<EAI_FAIL)|(1<<EAI_NODATA)}};
 
 static struct
 {
