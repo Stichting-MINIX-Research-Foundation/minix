b4e2783333cf7c697e72b0001a596cfee3aa4863
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2014-09-28 18:53:31 +0200

    sef: Naming refactory.
    Change-Id: Id313e73fde577e48a17f2c16c808c9156a1be804

--- minix/lib/libsys/sef_liveupdate.c
+++ minix/lib/libsys/sef_liveupdate.c
@@ -9,14 +9,14 @@ static int sef_lu_flags;
 extern __attribute__((weak)) int st_do_state_cleanup(void);
 
 /* SEF Live update callbacks. */
-static struct sef_cbs {
+static struct sef_lu_cbs {
     sef_cb_lu_prepare_t                 sef_cb_lu_prepare;
     sef_cb_lu_state_isvalid_t           sef_cb_lu_state_isvalid;
     sef_cb_lu_state_changed_t           sef_cb_lu_state_changed;
     sef_cb_lu_state_dump_t              sef_cb_lu_state_dump;
     sef_cb_lu_state_save_t              sef_cb_lu_state_save;
     sef_cb_lu_response_t                sef_cb_lu_response;
-} sef_cbs = {
+} sef_lu_cbs = {
     SEF_CB_LU_PREPARE_DEFAULT,
     SEF_CB_LU_STATE_ISVALID_DEFAULT,
     SEF_CB_LU_STATE_CHANGED_DEFAULT,
@@ -64,7 +64,7 @@ void do_sef_lu_before_receive(void)
   sef_lu_debug_begin();
   sef_lu_dprint("%s, cycle=%d. Dumping state variables:\n",
       sef_debug_header(), sef_lu_debug_cycle);
-  sef_cbs.sef_cb_lu_state_dump(sef_lu_state);
+  sef_lu_cbs.sef_cb_lu_state_dump(sef_lu_state);
   sef_lu_debug_end();
 #endif
 
@@ -73,7 +73,7 @@ void do_sef_lu_before_receive(void)
    */
   r = OK;
   if(sef_lu_state != SEF_LU_STATE_WORK_FREE) {
-      r = sef_cbs.sef_cb_lu_prepare(sef_lu_state);
+      r = sef_lu_cbs.sef_cb_lu_prepare(sef_lu_state);
   }
   if(r == OK) {
       sef_lu_ready(OK);
@@ -98,9 +98,9 @@ int do_sef_lu_request(message *m_ptr)
 
   /* Otherwise only accept live update requests with a valid state. */
   is_valid_state = SEF_LU_ALWAYS_ALLOW_DEBUG_STATES && SEF_LU_STATE_IS_DEBUG(state);
-  is_valid_state = is_valid_state || sef_cbs.sef_cb_lu_state_isvalid(state, flags);
+  is_valid_state = is_valid_state || sef_lu_cbs.sef_cb_lu_state_isvalid(state, flags);
   if(!is_valid_state) {
-      if(sef_cbs.sef_cb_lu_state_isvalid == SEF_CB_LU_STATE_ISVALID_DEFAULT) {
+      if(sef_lu_cbs.sef_cb_lu_state_isvalid == SEF_CB_LU_STATE_ISVALID_DEFAULT) {
           sef_lu_ready(ENOSYS);
       }
       else {
@@ -115,7 +115,7 @@ int do_sef_lu_request(message *m_ptr)
        * handle the rest.
        */
       if(old_state != sef_lu_state) {
-          sef_cbs.sef_cb_lu_state_changed(old_state, sef_lu_state);
+          sef_lu_cbs.sef_cb_lu_state_changed(old_state, sef_lu_state);
       }
   }
 
@@ -148,7 +148,7 @@ static void sef_lu_ready(int result)
       if (st_do_state_cleanup)
           r = st_do_state_cleanup();
       if(r == OK) {
-          r = sef_cbs.sef_cb_lu_state_save(sef_lu_state, sef_lu_flags);
+          r = sef_lu_cbs.sef_cb_lu_state_save(sef_lu_state, sef_lu_flags);
       }
       if(r != OK) {
           /* Abort update in case of error. */
@@ -164,7 +164,7 @@ static void sef_lu_ready(int result)
   m.m_type = RS_LU_PREPARE;
   m.m_rs_update.state = sef_lu_state;
   m.m_rs_update.result = result;
-  r = sef_cbs.sef_cb_lu_response(&m);
+  r = sef_lu_cbs.sef_cb_lu_response(&m);
 
 #if SEF_LU_DEBUG
   sef_lu_debug_begin();
@@ -181,7 +181,7 @@ static void sef_lu_ready(int result)
   old_state = sef_lu_state;
   sef_lu_state = SEF_LU_STATE_NULL;
   if(old_state != sef_lu_state) {
-      sef_cbs.sef_cb_lu_state_changed(old_state, sef_lu_state);
+      sef_lu_cbs.sef_cb_lu_state_changed(old_state, sef_lu_state);
   }
 }
 
@@ -191,7 +191,7 @@ static void sef_lu_ready(int result)
 void sef_setcb_lu_prepare(sef_cb_lu_prepare_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_prepare = cb;
+  sef_lu_cbs.sef_cb_lu_prepare = cb;
 }
 
 /*===========================================================================*
@@ -200,7 +200,7 @@ void sef_setcb_lu_prepare(sef_cb_lu_prepare_t cb)
 void sef_setcb_lu_state_isvalid(sef_cb_lu_state_isvalid_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_state_isvalid = cb;
+  sef_lu_cbs.sef_cb_lu_state_isvalid = cb;
 }
 
 /*===========================================================================*
@@ -209,7 +209,7 @@ void sef_setcb_lu_state_isvalid(sef_cb_lu_state_isvalid_t cb)
 void sef_setcb_lu_state_changed(sef_cb_lu_state_changed_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_state_changed = cb;
+  sef_lu_cbs.sef_cb_lu_state_changed = cb;
 }
 
 /*===========================================================================*
@@ -218,7 +218,7 @@ void sef_setcb_lu_state_changed(sef_cb_lu_state_changed_t cb)
 void sef_setcb_lu_state_dump(sef_cb_lu_state_dump_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_state_dump = cb;
+  sef_lu_cbs.sef_cb_lu_state_dump = cb;
 }
 
 /*===========================================================================*
@@ -227,7 +227,7 @@ void sef_setcb_lu_state_dump(sef_cb_lu_state_dump_t cb)
 void sef_setcb_lu_state_save(sef_cb_lu_state_save_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_state_save = cb;
+  sef_lu_cbs.sef_cb_lu_state_save = cb;
 }
 
 /*===========================================================================*
@@ -236,7 +236,7 @@ void sef_setcb_lu_state_save(sef_cb_lu_state_save_t cb)
 void sef_setcb_lu_response(sef_cb_lu_response_t cb)
 {
   assert(cb != NULL);
-  sef_cbs.sef_cb_lu_response = cb;
+  sef_lu_cbs.sef_cb_lu_response = cb;
 }
 
 /*===========================================================================*
@@ -315,7 +315,7 @@ int sef_cb_lu_prepare_never_ready(int UNUSED(state))
  *===========================================================================*/
 int sef_cb_lu_prepare_crash(int UNUSED(state))
 {
-  panic("Simulating a crash at update prepare time...");
+  panic("Simulating a crash at update prepare time...\n");
 
   return OK;
 }
