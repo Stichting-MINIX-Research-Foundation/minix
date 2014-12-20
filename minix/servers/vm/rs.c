0be084004f5c5f445622e19d003306e8ca5a54e7
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2014-12-20 18:14:27 +0100

    vm: Fix vm cloning.
    Change-Id: I996410ab1b9628cde797d9e5dd73ed07f13a884b

--- minix/servers/vm/rs.c
+++ minix/servers/vm/rs.c
@@ -168,11 +168,13 @@ static int rs_memctl_make_vm_instance(struct vmproc *new_vm_vmp)
 	 */
 	flags = 0;
 	verify = FALSE;
-	r = pt_ptalloc_in_range(&this_vm_vmp->vm_pt, 0, 0, flags, verify);
+	r = pt_ptalloc_in_range(&this_vm_vmp->vm_pt,
+		kernel_boot_info.freepde_start, ARCH_VM_DIR_ENTRIES, flags, verify);
 	if(r != OK) {
 		return r;
 	}
-	r = pt_ptalloc_in_range(&new_vm_vmp->vm_pt, 0, 0, flags, verify);
+	r = pt_ptalloc_in_range(&new_vm_vmp->vm_pt,
+		kernel_boot_info.freepde_start, ARCH_VM_DIR_ENTRIES, flags, verify);
 	if(r != OK) {
 		return r;
 	}
