145f267263b0f61a867f074688336275bda359fd
Jacob Adams <tookmund@gmail.com>
2015-06-07 16:42:31 -0400

    Begin condensing stuff

--- releasetools/netbsd2minix/run.sh
+++ releasetools/netbsd2minix/run.sh
@@ -12,14 +12,39 @@ SRC=$MINIX/../newsrc
 export MINIX N2M NETBSD SRC
 
 echo "Setup NetBSD"
-$N2M/setupnetbsd.sh
+cd $NETBSD || (git clone --depth 1 git://github.com/jsonn/src $NETBSD && cd $NETBSD)
+git pull
 
 echo "Clear new src tree"
 mkdir -p $SRC
 rm -rf $SRC/*
 
 echo "Apply whitelists"
-$N2M/whitelists.sh
+
+cd $N2M/whitelist
+for item in `ls`
+do
+	$N2M/applywhitelist.sh $NETBSD/$item $item < $item
+	echo "$item moved"
+done
+
+$N2M/applywhitelist.sh $MINIX . < $N2M/minix.txt
 
 echo "Apply special-cases"
-$N2M/special.sh
+
+cd $MINIX
+
+for dir in "external bin games gnu libexec sbin tools usr.bin usr.sbin"
+do
+	cp -f $MINIX/$dir/Makefile $SRC/$dir
+done
+
+cp -r $MINIX/include/cdbr.h $SRC/include/cdbr.h
+cp -r $MINIX/share/zoneinfo $SRC/share/zoneinfo
+cp -r $MINIX/tools/llvm-librt $SRC/tools/llvm-librt
+cp -r $MINIX/tools/mkfs.mfs $SRC/tools/mkfs.mfs
+cp -r $MINIX/tools/mkproto $SRC/tools/mkproto
+cp -r $MINIX/tools/partition $SRC/tools/partition
+cp -r $MINIX/tools/toproto $SRC/tools/toproto
+cp -r $MINIX/tools/writeisofs $SRC/tools/writeisofs
+
--- releasetools/netbsd2minix/setupnetbsd.sh
+++
@@ -1,2 +0,0 @@
-cd $NETBSD || (git clone --depth 1 git://github.com/jsonn/src $NETBSD && cd $NETBSD)
-git pull
