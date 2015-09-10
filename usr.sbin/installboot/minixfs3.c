9af3a794f610f29ae5d55b12a2ac8c10db9bc6af
Jean-Baptiste Boric <jblbeurope@gmail.com>
2015-09-11 00:02:37 +0200

    Add installboot to host tools
    Change-Id: I5594cfdb13362b51f86b9ef76e82c21402c47f8d

--- usr.sbin/installboot/minixfs3.c
+++ usr.sbin/installboot/minixfs3.c
@@ -3,8 +3,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <sys/stat.h>
-#include <sys/bootblock.h>
-#include <sys/syslimits.h>
+#include <limits.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
