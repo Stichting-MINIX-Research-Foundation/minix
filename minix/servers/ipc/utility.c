c6aad5d0d12dbbcc993ff1418ac39739a399168d
David van Moolenbroek <david@minix3.org>
2015-11-25 22:37:19 +0000

    IPC server: restyle
    Closer to KNF, better coding practices, more similar to other
    services, no more global variables, a few more comments, that
    kind of stuff.  No major functional changes.
    
    Change-Id: I6e8f53bfafd6f41e92031fba76c40a31d2107a8e

--- minix/servers/ipc/utility.c
+++ minix/servers/ipc/utility.c
@@ -1,35 +1,34 @@
 #include "inc.h"
 
-int check_perm(struct ipc_perm *req, endpoint_t who, int mode)
+int
+check_perm(struct ipc_perm * req, endpoint_t who, int mode)
 {
 	int req_mode;
 	int cur_mode;
-	uid_t uid = getnuid(who);
-	gid_t gid = getngid(who);
+	uid_t uid;
+	gid_t gid;
 
+	uid = getnuid(who);
+	gid = getngid(who);
 	mode &= 0666;
 
-	/* is root? */
+	/* Root is allowed to do anything. */
 	if (uid == 0)
 		return 1;
 
 	if (uid == req->uid || uid == req->cuid) {
-		/* same user */
+		/* Same user. */
 		req_mode = (req->mode >> 6) & 0x7;
 		cur_mode = (mode >> 6) & 0x7;
 	} else if (gid == req->gid || gid == req->cgid) {
-		/* same group */
+		/* Same group. */
 		req_mode = (req->mode >> 3) & 0x7;
 		cur_mode = (mode >> 3) & 0x7;
 	} else {
-		/* other group */
+		/* Other user and group. */
 		req_mode = req->mode & 0x7;
 		cur_mode = mode & 0x7;
 	}
 
-	if (cur_mode && ((cur_mode & req_mode) == cur_mode))
-		return 1;
-	else
-		return 0;
+	return (cur_mode && ((cur_mode & req_mode) == cur_mode));
 }
-
