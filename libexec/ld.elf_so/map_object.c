2f9e78557076e53390e33a45e370b4c974fc7321
Lionel Sambuc <lionel.sambuc@gmail.com>
2015-12-30 09:16:54 +0100

    ld.elf_so: Drop fallback routine
    Change-Id: Ia2236146c3302ba992aa6fea0fc9e9699fe9e3cd

--- libexec/ld.elf_so/map_object.c
+++ libexec/ld.elf_so/map_object.c
@@ -49,15 +49,6 @@ __RCSID("$NetBSD: map_object.c,v 1.53 2014/10/30 07:53:41 martin Exp $");
 #include "debug.h"
 #include "rtld.h"
 
-#if defined(__minix)
-#define MINIXVERBOSE 0
-
-#if MINIXVERBOSE
-#include <stdio.h>
-#endif
-
-#endif /* defined(__minix) */
-
 static int protflags(int);	/* Elf flags -> mmap protection */
 
 #define EA_UNDEF		(~(Elf_Addr)0)
@@ -114,9 +105,6 @@ _rtld_map_object(const char *path, int fd, const struct stat *sb)
 	caddr_t		 clear_addr;
 	size_t		 nclear;
 #endif
-#if defined(__minix)
-	Elf_Addr bsslen;
-#endif /* defined(__minix) */
 
 	if (sb != NULL && sb->st_size < (off_t)sizeof (Elf_Ehdr)) {
 		_rtld_error("%s: not ELF file (too short)", path);
@@ -135,12 +123,8 @@ _rtld_map_object(const char *path, int fd, const struct stat *sb)
 	    (off_t)0);
 	obj->ehdr = ehdr;
 	if (ehdr == MAP_FAILED) {
-#if defined(__minix)
-		return _rtld_map_object_fallback(path, fd, sb);
-#else
 		_rtld_error("%s: read error: %s", path, xstrerror(errno));
 		goto bad;
-#endif /* defined(__minix) */
 	}
 	/* Make sure the file is valid */
 	if (memcmp(ELFMAG, ehdr->e_ident, SELFMAG) != 0) {
@@ -373,12 +357,10 @@ _rtld_map_object(const char *path, int fd, const struct stat *sb)
 
 	/* Overlay the bss segment onto the proper region. */
 #if defined(__minix)
-	bsslen = base_vlimit - data_vlimit;
-	if (bsslen > 0 &&
-		mmap(mapbase + data_vlimit - base_vaddr, bsslen,
-#else
+	/* MINIX's mmap is strict and refuses 0-bytes mappings. */
+	if (0 < base_vlimit - data_vlimit)
+#endif /*defined(__minix) */
 	if (mmap(mapbase + data_vlimit - base_vaddr, base_vlimit - data_vlimit,
-#endif /* defined(__minix) */
 	    data_flags, MAP_ANON | MAP_PRIVATE | MAP_FIXED, -1, 0) ==
 	    MAP_FAILED) {
 		_rtld_error("mmap of bss failed: %s", xstrerror(errno));
