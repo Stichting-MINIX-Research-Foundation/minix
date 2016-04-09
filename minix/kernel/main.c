4cb315c48408e12c5833316974e0f43aca65e245
Jacob Adams <tookmund@gmail.com>
2016-04-08 23:43:11 -0400

    kernel: Update copyright year
    Change-Id: If566bf95db32d9f5ebf1a6677acdaad1c023112e

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -340,7 +340,7 @@ static void announce(void)
 #ifdef _VCS_REVISION
 	"(" _VCS_REVISION ")\n"
 #endif
-      "Copyright 2014, Vrije Universiteit, Amsterdam, The Netherlands\n",
+      "Copyright 2016, Vrije Universiteit, Amsterdam, The Netherlands\n",
       OS_RELEASE);
   printf("MINIX is open source software, see http://www.minix3.org\n");
 }
