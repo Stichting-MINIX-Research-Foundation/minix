4d99b215be7083a1d0d5c7a81fbd5e52e6d59eb0
Ralf Neeb <ralf.neeb@gmail.com>
2016-07-02 13:28:02 +0200

    made minix3 bootable via EFI

--- releasetools/x86_hdimage.sh
+++ releasetools/x86_hdimage.sh
@@ -26,6 +26,7 @@ fi
 : ${ROOT_SIZE=$((  128*(2**20) - ${BOOTXX_SECS} * 512 ))}
 : ${HOME_SIZE=$((  128*(2**20) ))}
 : ${USR_SIZE=$((  1792*(2**20) ))}
+: ${EFI_SIZE=$((  0  ))}
 
 # set up disk creation environment
 . releasetools/image.defaults
@@ -80,6 +81,18 @@ ROOTSIZEARG="-b $((${ROOT_SIZE} / 512 / 8))"
 USRSIZEARG="-b $((${USR_SIZE} / 512 / 8))"
 HOMESIZEARG="-b $((${HOME_SIZE} / 512 / 8))"
 
+if [ ${EFI_SIZE} -ge 512 ]
+then
+       fetch_and_build_grub
+
+       : ${EFI_DIR=$OBJ/efi}
+       rm -rf ${EFI_DIR} && mkdir -p ${EFI_DIR}/boot/minix_default ${EFI_DIR}/boot/efi
+       create_grub_cfg
+       cp ${MODDIR}/* ${EFI_DIR}/boot/minix_default/
+       cp ${RELEASETOOLSDIR}/grub/grub-core/booti386.efi ${EFI_DIR}/boot/efi
+       cp ${RELEASETOOLSDIR}/grub/grub-core/*.mod ${EFI_DIR}/boot/efi
+fi
+
 ROOT_START=${BOOTXX_SECS}
 echo " * ROOT"
 _ROOT_SIZE=$(${CROSS_TOOLS}/nbmkfs.mfs -d ${ROOTSIZEARG} -I $((${ROOT_START}*512)) ${IMG} ${WORK_DIR}/proto.root)
@@ -97,7 +110,18 @@ _HOME_SIZE=$(($_HOME_SIZE / 512))
 # Write the partition table using the natively compiled
 # minix partition utility
 #
-${CROSS_TOOLS}/nbpartition -m ${IMG} ${BOOTXX_SECS} 81:${_ROOT_SIZE} 81:${_USR_SIZE} 81:${_HOME_SIZE}
+if [ ${EFI_SIZE} -ge 512 ]
+then
+       dd if=/dev/zero bs=${EFI_SIZE} count=1 > ${OBJ}/efi.img
+       EFI_START=$((${HOME_START} + ${_HOME_SIZE}))
+       echo " * EFI"
+       ${CROSS_TOOLS}/nbmakefs -t msdos -s ${EFI_SIZE} -o "F=32,c=1" ${OBJ}/efi.img ${EFI_DIR}
+       dd if=${OBJ}/efi.img >> ${IMG}
+       ${CROSS_TOOLS}/nbpartition -m ${IMG} ${BOOTXX_SECS} 81:${_ROOT_SIZE}* 81:${_USR_SIZE} 81:${_HOME_SIZE} EF:1+
+else
+       ${CROSS_TOOLS}/nbpartition -m ${IMG} ${BOOTXX_SECS} 81:${_ROOT_SIZE}* 81:${_USR_SIZE} 81:${_HOME_SIZE}
+fi
+
 ${CROSS_TOOLS}/nbinstallboot -f -m ${ARCH} ${IMG} ${DESTDIR}/usr/mdec/bootxx_minixfs3
 
 echo ""
@@ -108,3 +132,5 @@ echo "qemu-system-i386 --enable-kvm -m 256 -hda `pwd`/${IMG}"
 echo ""
 echo "To boot this image on kvm:"
 echo "cd ${MODDIR} && qemu-system-i386 --enable-kvm -m 256M -kernel kernel -append \"rootdevname=c0d0p0\" -initrd \"${mods}\" -hda `pwd`/${IMG}"
+echo "To boot this image on kvm with EFI (tianocore OVMF):"
+echo "qemu-system-i386 -L . -bios OVMF-i32.fd -m 256M -drive file=minix_x86.img,if=ide,format=raw"
