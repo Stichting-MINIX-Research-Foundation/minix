9490732a59360e701fac2b5d3f27a515c7c7fb5e
David van Moolenbroek <david@minix3.org>
2017-02-14 17:50:02 +0000

    Retire MINIX dhcpd(8)
    Change-Id: I4b8c738b6176ce390a7a7817b0dcaf9caffe636c

--- minix/include/minix/paths.h
+++ minix/include/minix/paths.h
@@ -8,11 +8,6 @@
 #define _PATH_SYSTEM_CONF_DIR "/etc/system.conf.d"
 #define _PATH_SYSTEM_CONF_PKG_DIR       "/usr/pkg/etc/system.conf.d"
 
-#define _PATH_DHCPCONF	"/etc/dhcp.conf"
-#define _PATH_DHCPPID	"/usr/run/dhcpd.pid"
-#define _PATH_DHCPCACHE	"/usr/adm/dhcp.cache"
-#define _PATH_DHCPPOOL	"/usr/adm/dhcp.pool"
-
 #define _PATH_RAMDISK	"/dev/ram"
 
 #define _PATH_DRIVERS	"/service"
